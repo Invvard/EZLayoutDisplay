cfeda221cbdf4348b8e69176e26ad0aee53911c5
Pierre C <invvard+git@outlook.com>
2019-01-12 19:44:55 -0500

    Add the capability to start directly in the tray

--- InvvardDev.ErgodoxLayoutDisplay.Desktop/App.xaml.cs
+++ InvvardDev.ErgodoxLayoutDisplay.Desktop/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using GalaSoft.MvvmLight.Threading;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace InvvardDev.ErgodoxLayoutDisplay.Desktop
 {
@@ -8,9 +9,25 @@ namespace InvvardDev.ErgodoxLayoutDisplay.Desktop
     /// </summary>
     public partial class App : Application
     {
+        private TaskbarIcon _notifyIcon;
+
         static App()
         {
             DispatcherHelper.Initialize();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //create the notifyicon (it's a resource declared in NotifyIconResources.xaml
+            _notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _notifyIcon.Dispose(); //the icon would clean up automatically, but this is cleaner
+            base.OnExit(e);
         }
     }
 }
