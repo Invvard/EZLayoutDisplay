735b85c76caa7bbb6426014a9f624ba3f34d14b5
Invvard <7305493+Invvard@users.noreply.github.com>
2019-01-13 22:15:26 -0500

    Dynamise the Window Title

--- InvvardDev.ErgodoxLayoutDisplay.Desktop/App.xaml.cs
+++ InvvardDev.ErgodoxLayoutDisplay.Desktop/App.xaml.cs
@@ -1,31 +1,36 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Navigation;
+﻿using System.Windows;
 using GalaSoft.MvvmLight.Threading;
 using Hardcodet.Wpf.TaskbarNotification;
+using InvvardDev.ErgodoxLayoutDisplay.Desktop.Service.Implementation;
+using InvvardDev.ErgodoxLayoutDisplay.Desktop.Service.Interface;
 
 namespace InvvardDev.ErgodoxLayoutDisplay.Desktop
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
-        static App()
+        private TaskbarIcon _notifyIcon;
+        private IKeyboardListenerService _keyboardListenerService;
+
+        public App()
         {
             DispatcherHelper.Initialize();
+            _keyboardListenerService = new KeyboardListenerService();
         }
-        private TaskbarIcon _notifyIcon;
 
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
             //create the notifyicon (it's a resource declared in NotifyIconResources.xaml
             _notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
+            
         }
 
         protected override void OnExit(ExitEventArgs e)
         {
+            _keyboardListenerService.Dispose();
             _notifyIcon.Dispose(); //the icon would clean up automatically, but this is cleaner
             base.OnExit(e);
         }
