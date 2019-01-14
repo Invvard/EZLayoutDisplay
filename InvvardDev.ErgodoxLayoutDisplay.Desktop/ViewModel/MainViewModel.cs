07c0449297f4777b4626e4d66bc19d5c84ef2552
Invvard <7305493+Invvard@users.noreply.github.com>
2019-01-13 22:14:59 -0500

    Add a dynamic welcome title

--- InvvardDev.ErgodoxLayoutDisplay.Desktop/ViewModel/MainViewModel.cs
+++ InvvardDev.ErgodoxLayoutDisplay.Desktop/ViewModel/MainViewModel.cs
@@ -10,11 +10,24 @@ namespace InvvardDev.ErgodoxLayoutDisplay.Desktop.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
+        private string _welcomeTitle = string.Empty;
+
+        /// <summary>
+        /// Gets the WelcomeTitle property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public string WelcomeTitle
+        {
+            get => _welcomeTitle;
+            set => Set(ref _welcomeTitle, value);
+        }
+
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
         public MainViewModel()
         {
+            WelcomeTitle = "Ergodox Layout Display";
         }
         
         ////public override void Cleanup()
