fb0a6cf082f3eba3d65d2cbff12014b4dbb135c1
Invvard <7305493+Invvard@users.noreply.github.com>
2019-01-14 21:59:58 -0500

    Change the way the app is initialized
    Now, the MainWindow is hidden completely and hold the NotificationIcon.

--- InvvardDev.ErgodoxLayoutDisplay.Desktop/App.xaml.cs
+++ InvvardDev.ErgodoxLayoutDisplay.Desktop/App.xaml.cs
@@ -13,20 +13,5 @@ namespace InvvardDev.ErgodoxLayoutDisplay.Desktop
         {
             DispatcherHelper.Initialize();
         }
-        private TaskbarIcon _notifyIcon;
-
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-
-            //create the notifyicon (it's a resource declared in NotifyIconResources.xaml
-            _notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
-        }
-
-        protected override void OnExit(ExitEventArgs e)
-        {
-            _notifyIcon.Dispose(); //the icon would clean up automatically, but this is cleaner
-            base.OnExit(e);
-        }
     }
 }
--- InvvardDev.ErgodoxLayoutDisplay.Desktop/ViewModel/MainViewModel.cs
+++ InvvardDev.ErgodoxLayoutDisplay.Desktop/ViewModel/MainViewModel.cs
@@ -14,13 +14,37 @@ namespace InvvardDev.ErgodoxLayoutDisplay.Desktop.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
+        private ICommand _showWindowCommand;
+        private ICommand _exitCommand;
+
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
         public MainViewModel()
         {
         }
-        
+
+        /// <summary>
+        /// Shows a window, if none is already open.
+        /// </summary>
+        public ICommand ShowWindowCommand =>
+            _showWindowCommand
+            ?? (_showWindowCommand = new RelayCommand(() =>
+                                                      {
+                                                          if (Application.Current.MainWindow == null)
+                                                          {
+                                                              Application.Current.MainWindow = new MainWindow();
+                                                          }
+                                                          Application.Current.MainWindow.Show();
+                                                      }));
+
+        /// <summary>
+        /// Shuts down the application.
+        /// </summary>
+        public ICommand ExitApplicationCommand =>
+            _exitCommand
+            ?? (_exitCommand = new RelayCommand(() => Application.Current.Shutdown()));
+
         ////public override void Cleanup()
         ////{
         ////    // Clean up if needed
