aaf7c8a5d332e001e04c3b8d9f14e5ce2a45a3d7
Pierre C <invvard+git@outlook.com>
2019-01-15 21:27:34 -0500

    Add the IWindowService and its implementation

---
+++ InvvardDev.ErgodoxLayoutDisplay.Desktop/Model/Service/Implementation/WindowService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using InvvardDev.ErgodoxLayoutDisplay.Desktop.Model.Service.Interface;
+
+namespace InvvardDev.ErgodoxLayoutDisplay.Desktop.Model.Service.Implementation
+{
+    public class WindowService: IWindowService
+    {
+        private Dictionary<string, Window> _windows;
+
+        public WindowService()
+        {
+            _windows = new Dictionary<string, Window>();
+        }
+
+        public void ShowWindow<T>()
+            where T : Window, new()
+        {
+            var windowKey = typeof(T).ToString();
+            if (!_windows.ContainsKey(windowKey))
+            {
+                _windows.Add(windowKey, new T());
+            }
+
+            _windows[windowKey].Show();
+        }
+    }
+}
