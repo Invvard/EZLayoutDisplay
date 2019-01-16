57f505f0cad73f9059d61d8e9e3b7c071634e60d
Invvard <7305493+Invvard@users.noreply.github.com>
2019-01-15 22:16:37 -0500

    Bind the View and ViewModel

--- InvvardDev.ErgodoxLayoutDisplay.Desktop/ViewModel/SettingsViewModel.cs
+++ InvvardDev.ErgodoxLayoutDisplay.Desktop/ViewModel/SettingsViewModel.cs
@@ -4,6 +4,17 @@ namespace InvvardDev.ErgodoxLayoutDisplay.Desktop.ViewModel
 {
     public class SettingsViewModel : ViewModelBase
     {
-        
+        private string _windowTitle;
+
+        public string WindowTitle
+        {
+            get => _windowTitle;
+            set => Set(ref _windowTitle, value);
+        }
+
+        public SettingsViewModel()
+        {
+            WindowTitle = "Settings";
+        }
     }
 }
