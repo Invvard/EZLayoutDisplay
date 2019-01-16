dad882401c161c4bc55c4994619a39ef3d6a986f
Pierre C <invvard+git@outlook.com>
2019-01-15 20:58:16 -0500

    Open the LayoutDisplay window from the TrayMenu

--- InvvardDev.ErgodoxLayoutDisplay.Desktop/ViewModel/DisplayLayoutViewModel.cs
+++ InvvardDev.ErgodoxLayoutDisplay.Desktop/ViewModel/DisplayLayoutViewModel.cs
@@ -4,6 +4,17 @@ namespace InvvardDev.ErgodoxLayoutDisplay.Desktop.ViewModel
 {
     public class DisplayLayoutViewModel : ViewModelBase
     {
-        
+        private string _windowTitle;
+
+        public string WindowTitle
+        {
+            get => _windowTitle;
+            set { Set(() => WindowTitle, ref _windowTitle, value); }
+        }
+
+        public DisplayLayoutViewModel()
+        {
+            WindowTitle = "Ergodox Layout";
+        }
     }
 }
