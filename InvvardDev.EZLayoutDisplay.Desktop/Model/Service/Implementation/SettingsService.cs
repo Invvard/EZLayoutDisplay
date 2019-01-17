32e7063c6985bf0d7c1484af28a7c0e9e0a720bc
Invvard <7305493+Invvard@users.noreply.github.com>
2019-01-16 21:07:40 -0500

    Readonly on the _settings field

--- InvvardDev.EZLayoutDisplay.Desktop/Model/Service/Implementation/SettingsService.cs
+++ InvvardDev.EZLayoutDisplay.Desktop/Model/Service/Implementation/SettingsService.cs
@@ -1,12 +1,11 @@
-﻿using System.Windows;
-using InvvardDev.EZLayoutDisplay.Desktop.Model.Service.Interface;
+﻿using InvvardDev.EZLayoutDisplay.Desktop.Model.Service.Interface;
 using InvvardDev.EZLayoutDisplay.Desktop.Properties;
 
 namespace InvvardDev.EZLayoutDisplay.Desktop.Model.Service.Implementation
 {
     internal class SettingsService : ISettingsService
     {
-        private Settings _settings;
+        private readonly Settings _settings;
 
         public SettingsService(Settings settings)
         {
