7b9ad4c3f1d0e0fc5db6994b9ff9169c65041f4c
Pierre C <invvard+git@outlook.com>
2019-01-17 10:58:24 -0500

    Add the Hotkey model

---
+++ InvvardDev.EZLayoutDisplay.Desktop/Model/Hotkey.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Windows.Documents;
+using NonInvasiveKeyboardHookLibrary;
+
+namespace InvvardDev.EZLayoutDisplay.Desktop.Model
+{
+    public class Hotkey
+    {
+        /// <summary>
+        /// Gets or sets the list of <see cref="ModifierKeys"/>.
+        /// </summary>
+        public List<ModifierKeys> ModifierKeys { get; set; }
+
+        /// <summary>
+        /// Gets or sets the complementary key code.
+        /// </summary>
+        public string KeyCode { get; set; }
+    }
+}
