710ead348b1119ef2e9dc24fa98c13be0409d81a
Invvard <7305493+Invvard@users.noreply.github.com>
2019-01-17 18:32:50 -0500

    Add an Enum for Settings names

---
+++ InvvardDev.EZLayoutDisplay.Desktop/Model/Enum/SettingsName.cs
@@ -0,0 +1,9 @@
+﻿namespace InvvardDev.EZLayoutDisplay.Desktop.Model.Enum
+{
+    public static class SettingsName
+
+    {
+        public static string HotkeyShowLayout => "HotkeyShowLayout";
+        public static string ErgodoxLayoutUrl => "ErgodoxLayoutUrl";
+    }
+}
