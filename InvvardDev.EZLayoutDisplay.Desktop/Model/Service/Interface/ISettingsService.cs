0710070999d342877d7c2fe6962b0e343d952267
Pierre C <invvard+git@outlook.com>
2019-01-17 18:34:21 -0500

    Simplifies the SettingsService

--- InvvardDev.EZLayoutDisplay.Desktop/Model/Service/Interface/ISettingsService.cs
+++ InvvardDev.EZLayoutDisplay.Desktop/Model/Service/Interface/ISettingsService.cs
@@ -1,25 +1,19 @@
-﻿namespace InvvardDev.EZLayoutDisplay.Desktop.Model.Service.Interface
+﻿using System;
+
+namespace InvvardDev.EZLayoutDisplay.Desktop.Model.Service.Interface
 {
     public interface ISettingsService
     {
-        /// <summary>
-        /// Updates a setting value.
-        /// </summary>
-        /// <param name="key">Key to target.</param>
-        /// <param name="value">Value to update.</param>
-        void UpdateSetting(string key, string value);
-
-        /// <summary>
-        /// Gets a setting value.
-        /// </summary>
-        /// <param name="key">Key to target.</param>
-        /// <returns>The wanted value.</returns>
-        string GetSetting(string key);
-
         /// <summary>
         /// Gets the Show Layout Hotkey.
         /// </summary>
         /// <returns>The hotkey from Settings.</returns>
         Hotkey GetHotKeyShowLayout();
+        
+        /// <summary>
+        /// Gets the Ergodox Layout URL.
+        /// </summary>
+        /// <returns>The URL to the Ergodox Layout.</returns>
+        string GetErgodoxLayoutUrl();
     }
 }
