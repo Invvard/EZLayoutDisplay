2efcaa08f76c73049e0c209c5902ed7c0a013c94
Invvard <7305493+Invvard@users.noreply.github.com>
2019-01-16 22:20:46 -0500

    Add the Design SettingsService

---
+++ InvvardDev.EZLayoutDisplay.Desktop/Design/SettingsService.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using InvvardDev.EZLayoutDisplay.Desktop.Model.Service.Interface;
+
+namespace InvvardDev.EZLayoutDisplay.Desktop.Design
+{
+    public class SettingsService : ISettingsService
+    {
+        public void UpdateKey(string key, string value)
+        {
+            Debug.WriteLine($"{key} key updated.");
+        }
+    }
+}
