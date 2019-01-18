a82188120d3a7fcaeb5eb9c7511fcaf7dee12aee
Pierre C <invvard+git@outlook.com>
2019-01-17 22:58:26 -0500

    Add the Design time WindowService

---
+++ InvvardDev.EZLayoutDisplay.Desktop/Design/WindowService.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using InvvardDev.EZLayoutDisplay.Desktop.Model.Service.Interface;
+
+namespace InvvardDev.EZLayoutDisplay.Desktop.Design
+{
+    public class WindowService : IWindowService
+    {
+        public void ShowWindow<T>()
+            where T : Window, new()
+        {
+            Debug.WriteLine($"Opens window {typeof(T)}");
+        }
+
+        public void CloseWindow<T>()
+        {
+            Debug.WriteLine($"Closes window {typeof(T)}");
+        }
+    }
+}
