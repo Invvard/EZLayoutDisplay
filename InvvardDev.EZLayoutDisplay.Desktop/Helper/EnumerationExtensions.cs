1ccf9649cc2c0e865ca81126048be2e733d3fb33
Invvard <7305493+Invvard@users.noreply.github.com>
2019-01-17 22:27:47 -0500

    Add hotkey registering from stored setting

---
+++ InvvardDev.EZLayoutDisplay.Desktop/Helper/EnumerationExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace InvvardDev.EZLayoutDisplay.Desktop.Helper
+{
+    public static class EnumerationExtensions
+    {
+        public static bool Has<T>(this Enum type, T value)
+        {
+            try { return ((int) (object) type & (int) (object) value) == (int) (object) value; }
+            catch { return false; }
+        }
+
+        public static bool Is<T>(this Enum type, T value)
+        {
+            try { return (int) (object) type == (int) (object) value; }
+            catch { return false; }
+        }
+
+        public static T Add<T>(this Enum type, T value)
+        {
+            try { return (T) (object) ((int) (object) type | (int) (object) value); }
+            catch (Exception ex) { throw new ArgumentException($"Could not append value from enumerated type '{typeof(T).Name}'.", ex); }
+        }
+
+        public static T Remove<T>(this Enum type, T value)
+        {
+            try { return (T) (object) ((int) (object) type & ~(int) (object) value); }
+            catch (Exception ex)
+            {
+                throw new ArgumentException($"Could not remove value from enumerated type '{typeof(T).Name}'.", ex);
+            }
+        }
+    }
+}
