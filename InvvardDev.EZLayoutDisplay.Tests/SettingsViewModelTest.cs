09f8862aa390c6df704c0fa4e0374fa8dfb156ab
Pierre C <invvard+git@outlook.com>
2019-01-18 19:58:48 -0500

    Add the Unit test project

---
+++ InvvardDev.EZLayoutDisplay.Tests/SettingsViewModelTest.cs
@@ -0,0 +1,26 @@
+﻿using InvvardDev.EZLayoutDisplay.Desktop.Model.Service.Interface;
+using InvvardDev.EZLayoutDisplay.Desktop.ViewModel;
+using Moq;
+using Xunit;
+
+namespace InvvardDev.EZLayoutDisplay.Tests
+{
+    public class SettingsViewModelTest
+    {
+        [Fact]
+        public void SettingsViewModelConstructor()
+        {
+            //Arrange
+            var tbContent = "This is a test";
+            var settingsService = new Mock<ISettingsService>();
+            settingsService.Setup(s => s.ErgodoxLayoutUrl).Returns(tbContent);
+            var windowService = new Mock<IWindowService>();
+
+            //Act
+            var settingsViewModel = new SettingsViewModel(settingsService.Object, windowService.Object);
+
+            //Assert
+            Assert.Equal(tbContent, settingsViewModel.TxtLayoutUrlText);
+        }
+    }
+}
