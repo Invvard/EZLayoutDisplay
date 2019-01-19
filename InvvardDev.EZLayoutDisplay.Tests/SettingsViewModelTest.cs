068a712d6733233696f0db8ec2b1a818d28fa00b
Pierre C <invvard+git@outlook.com>
2019-01-18 20:08:46 -0500

    Add test on relay command CanExecute status

--- InvvardDev.EZLayoutDisplay.Tests/SettingsViewModelTest.cs
+++ InvvardDev.EZLayoutDisplay.Tests/SettingsViewModelTest.cs
@@ -22,5 +22,34 @@ namespace InvvardDev.EZLayoutDisplay.Tests
             //Assert
             Assert.Equal(tbContent, settingsViewModel.TxtLayoutUrlText);
         }
+
+        [Theory]
+        [InlineData("initial value", "initial value", false)]
+        [InlineData("initial value", "new value", true)]
+        public void RelayCommand_CanExecute(string initialValue, string newValue, bool canExecute)
+        {
+            //Arrange
+            var settingsService = new Mock<ISettingsService>();
+            settingsService.Setup(s => s.ErgodoxLayoutUrl).Returns(initialValue);
+            var windowService = new Mock<IWindowService>();
+
+            //Act
+            var settingsViewModel = new SettingsViewModel(settingsService.Object, windowService.Object);
+            settingsViewModel.TxtLayoutUrlText = newValue;
+
+            //Assert
+            if (canExecute)
+            {
+                Assert.True(settingsViewModel.ApplySettingsCommand.CanExecute(null));
+                Assert.True(settingsViewModel.CancelSettingsCommand.CanExecute(null));
+            }
+            else
+            {
+                Assert.False(settingsViewModel.ApplySettingsCommand.CanExecute(null));
+                Assert.False(settingsViewModel.CancelSettingsCommand.CanExecute(null));
+            }
+
+            Assert.True(settingsViewModel.CloseSettingsCommand.CanExecute(null));
+        }
     }
 }
