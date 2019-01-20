b12b7c60e8639a952f8ddf3f213f2e296f43faf4
Pierre C <invvard+git@outlook.com>
2019-01-20 00:55:27 -0500

    Change Project name

--- InvvardDev.EZLayoutDisplay.Desktop/Properties/AssemblyInfo.cs
+++ InvvardDev.EZLayoutDisplay.Desktop/Properties/AssemblyInfo.cs
@@ -1,18 +1,16 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InvvardDev.ErgodoxLayoutDisplay")]
+[assembly: AssemblyTitle("InvvardDev.EZLayoutDisplay")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("InvvardDev.ErgodoxLayoutDisplay")]
+[assembly: AssemblyProduct("InvvardDev.EZLayoutDisplay")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © InvvardDev 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
