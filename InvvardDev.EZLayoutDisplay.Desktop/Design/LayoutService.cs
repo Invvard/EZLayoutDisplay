80adefafad7e46416acfe22e6b06047eb717edc3
Pierre C <invvard+git@outlook.com>
2019-01-21 21:54:12 -0500

    Reference the ILayoutService

--- InvvardDev.EZLayoutDisplay.Desktop/Design/LayoutService.cs
+++ InvvardDev.EZLayoutDisplay.Desktop/Design/LayoutService.cs
@@ -1,9 +1,17 @@
-﻿using InvvardDev.EZLayoutDisplay.Desktop.Model.Service.Interface;
+﻿using System.Diagnostics;
+using InvvardDev.EZLayoutDisplay.Desktop.Model;
+using InvvardDev.EZLayoutDisplay.Desktop.Model.Service.Interface;
 
 namespace InvvardDev.EZLayoutDisplay.Desktop.Design
 {
     public class LayoutService : ILayoutService
     {
-        
+        /// <inheritdoc />
+        public EZLayout GetErgodoxLayout(string layoutUrl)
+        {
+            Debug.WriteLine("Layout retrieved.");
+
+            return new EZLayout();
+        }
     }
 }
--- InvvardDev.EZLayoutDisplay.Desktop/Model/Service/Implementation/LayoutService.cs
+++ InvvardDev.EZLayoutDisplay.Desktop/Model/Service/Implementation/LayoutService.cs
@@ -4,6 +4,12 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.Model.Service.Implementation
 {
     public class LayoutService : ILayoutService
     {
-        
+        /// <inheritdoc />
+        public EZLayout GetErgodoxLayout(string layoutUrl)
+        {
+            var ezLayout = new EZLayout();
+
+            return ezLayout;
+        }
     }
 }
