0b26147ba0e02fd02082d2db3286b67d2f8827d0
Invvard <7305493+Invvard@users.noreply.github.com>
2019-01-21 21:54:12 -0500

    Reference the ILayoutService

--- InvvardDev.EZLayoutDisplay.Desktop/Model/Service/Interface/ILayoutService.cs
+++ InvvardDev.EZLayoutDisplay.Desktop/Model/Service/Interface/ILayoutService.cs
@@ -2,6 +2,11 @@
 {
     public interface ILayoutService
     {
-        
+        /// <summary>
+        /// Gets the <see cref="ErgodoxLayout"/>.
+        /// </summary>
+        /// <param name="layoutUrl">The layout URL to get.</param>
+        /// <returns>The prepared EZLayout.</returns>
+        EZLayout GetErgodoxLayout(string layoutUrl);
     }
 }
