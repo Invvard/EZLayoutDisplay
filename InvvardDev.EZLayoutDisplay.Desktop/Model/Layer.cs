2ad1f8b9a0de7455d4a4572bba4739648a05ca61
Pierre C <invvard+git@outlook.com>
2019-01-21 20:58:33 -0500

    Add the Ergodox Layout Configurator model.

---
+++ InvvardDev.EZLayoutDisplay.Desktop/Model/Layer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace InvvardDev.EZLayoutDisplay.Desktop.Model {
+    public class Layer
+    {
+        /// <summary>
+        /// Gets or sets the layer hash identifier.
+        /// </summary>
+        public string HashId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the layer's list of keys.
+        /// </summary>
+        public List<Key> Keys { get; set; }
+
+        /// <summary>
+        /// Gets or sets the layer's position.
+        /// </summary>
+        public int Position { get; set; }
+
+        /// <summary>
+        /// Gets or sets the layer's title.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the layer's color.
+        /// </summary>
+        public string Color { get; set; }
+    }
+}
---
+++ InvvardDev.EZLayoutDisplay.Desktop/Model/Revision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace InvvardDev.EZLayoutDisplay.Desktop.Model {
+    public class Revision
+    {
+        /// <summary>
+        /// Gets or sets the revision hash identifier.
+        /// </summary>
+        public string HashId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the keyboard model.
+        /// </summary>
+        public string Model { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the list of <see cref="Layer"/>.
+        /// </summary>
+        public List<Layer> Layers { get; set; }
+    }
+}
