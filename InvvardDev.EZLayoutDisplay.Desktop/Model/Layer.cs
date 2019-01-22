36b5d5e95aa636fb3656601a53fe798c1d63c348
Pierre C <invvard+git@outlook.com>
2019-01-21 21:15:27 -0500

    Add the JsonPropertyAttribute

--- InvvardDev.EZLayoutDisplay.Desktop/Model/Layer.cs
+++ InvvardDev.EZLayoutDisplay.Desktop/Model/Layer.cs
@@ -1,31 +1,38 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
-namespace InvvardDev.EZLayoutDisplay.Desktop.Model {
+namespace InvvardDev.EZLayoutDisplay.Desktop.Model
+{
     public class Layer
     {
         /// <summary>
         /// Gets or sets the layer hash identifier.
         /// </summary>
+        [JsonProperty("hashId")]
         public string HashId { get; set; }
 
         /// <summary>
-        /// Gets or sets the layer's list of keys.
+        /// Gets or sets the layer's list of <see cref="Key"/>.
         /// </summary>
+        [JsonProperty("keys")]
         public List<Key> Keys { get; set; }
 
         /// <summary>
         /// Gets or sets the layer's position.
         /// </summary>
+        [JsonProperty("position")]
         public int Position { get; set; }
 
         /// <summary>
         /// Gets or sets the layer's title.
         /// </summary>
+        [JsonProperty("title")]
         public string Title { get; set; }
 
         /// <summary>
         /// Gets or sets the layer's color.
         /// </summary>
+        [JsonProperty("color")]
         public string Color { get; set; }
     }
 }
--- InvvardDev.EZLayoutDisplay.Desktop/Model/Layout.cs
+++ InvvardDev.EZLayoutDisplay.Desktop/Model/Layout.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace InvvardDev.EZLayoutDisplay.Desktop.Model
 {
@@ -7,16 +8,19 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.Model
         /// <summary>
         /// Gets or sets the layout hash identifier.
         /// </summary>
+        [JsonProperty("hashId")]
         public string HashId { get; set; }
 
         /// <summary>
         /// Gets or sets the layout title.
         /// </summary>
+        [JsonProperty("title")]
         public string Title { get; set; }
 
         /// <summary>
         /// Gets or sets the list of <see cref="Revision"/>.
         /// </summary>
+        [JsonProperty("revisions")]
         public List<Revision> Revisions { get; set; }
     }
 }
