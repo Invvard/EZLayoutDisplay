6e0d0b25c38b8691113eb92e71fdf328ba1e6aad
Invvard <7305493+Invvard@users.noreply.github.com>
2019-01-21 21:15:27 -0500

    Add the JsonPropertyAttribute

--- InvvardDev.EZLayoutDisplay.Desktop/Model/Modifiers.cs
+++ InvvardDev.EZLayoutDisplay.Desktop/Model/Modifiers.cs
@@ -1,44 +1,55 @@
-﻿namespace InvvardDev.EZLayoutDisplay.Desktop.Model {
+﻿using Newtonsoft.Json;
+
+namespace InvvardDev.EZLayoutDisplay.Desktop.Model
+{
     public class Modifiers
     {
         /// <summary>
         /// Gets or sets the Left Alt modifier indicator.
         /// </summary>
+        [JsonProperty("leftAlt")]
         public bool LeftAlt { get; set; }
 
         /// <summary>
         /// Gets or sets the Left Cmd/Win modifier indicator.
         /// </summary>
-        public bool LeftGui { get; set; }
+        [JsonProperty("leftGui")]
+        public bool LeftWin { get; set; }
 
         /// <summary>
         /// Gets or sets the Left Ctrl modifier indicator.
         /// </summary>
+        [JsonProperty("leftCtrl")]
         public bool LeftCtrl { get; set; }
 
         /// <summary>
         /// Gets or sets the Right Alt modifier indicator.
         /// </summary>
+        [JsonProperty("rightAlt")]
         public bool RightAlt { get; set; }
 
         /// <summary>
         /// Gets or sets the Right Cmd/Win modifier indicator.
         /// </summary>
-        public bool RightGui { get; set; }
+        [JsonProperty("rightGui")]
+        public bool RightWin { get; set; }
 
         /// <summary>
         /// Gets or sets the Left Shift modifier indicator.
         /// </summary>
+        [JsonProperty("leftShift")]
         public bool LeftShift { get; set; }
 
         /// <summary>
         /// Gets or sets the Right Ctrl modifier indicator.
         /// </summary>
+        [JsonProperty("rightCtrl")]
         public bool RightCtrl { get; set; }
 
         /// <summary>
         /// Gets or sets the Right Shift modifier indicator.
         /// </summary>
+        [JsonProperty("rightShift")]
         public bool RightShift { get; set; }
     }
 }
--- InvvardDev.EZLayoutDisplay.Desktop/Model/Revision.cs
+++ InvvardDev.EZLayoutDisplay.Desktop/Model/Revision.cs
@@ -1,21 +1,26 @@
 ﻿using System.Collections.Generic;
+using Newtonsoft.Json;
 
-namespace InvvardDev.EZLayoutDisplay.Desktop.Model {
+namespace InvvardDev.EZLayoutDisplay.Desktop.Model
+{
     public class Revision
     {
         /// <summary>
         /// Gets or sets the revision hash identifier.
         /// </summary>
+        [JsonProperty("hashId")]
         public string HashId { get; set; }
 
         /// <summary>
         /// Gets or sets the keyboard model.
         /// </summary>
+        [JsonProperty("model")]
         public string Model { get; set; }
-        
+
         /// <summary>
         /// Gets or sets the list of <see cref="Layer"/>.
         /// </summary>
+        [JsonProperty("layers")]
         public List<Layer> Layers { get; set; }
     }
 }
