88c6b03c314d24d2e9a45d384cead9cdff8b097e
Invvard <7305493+Invvard@users.noreply.github.com>
2019-01-21 20:58:14 -0500

    Add the ILayoutService interface & implementations

---
+++ InvvardDev.EZLayoutDisplay.Desktop/Design/LayoutService.cs
@@ -0,0 +1,9 @@
+﻿using InvvardDev.EZLayoutDisplay.Desktop.Model.Service.Interface;
+
+namespace InvvardDev.EZLayoutDisplay.Desktop.Design
+{
+    public class LayoutService : ILayoutService
+    {
+        
+    }
+}
