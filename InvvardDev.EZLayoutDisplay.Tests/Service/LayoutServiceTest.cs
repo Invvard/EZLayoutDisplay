5a9ed9aca1ac4c5e4dd17527e4ac5d0831b43da7
Invvard <7305493+Invvard@users.noreply.github.com>
2019-01-22 19:41:20 -0500

    Unit test the GetErgodoxLayout

---
+++ InvvardDev.EZLayoutDisplay.Tests/Service/LayoutServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using InvvardDev.EZLayoutDisplay.Desktop.Model;
+using InvvardDev.EZLayoutDisplay.Desktop.Model.Service.Implementation;
+using InvvardDev.EZLayoutDisplay.Desktop.Model.Service.Interface;
+using Xunit;
+
+namespace InvvardDev.EZLayoutDisplay.Tests.Service
+{
+    public class LayoutServiceTest
+    {
+        [ Fact ]
+        public async Task GetErgodoxLayout()
+        {
+            // Arrange
+            ILayoutService layoutService = new LayoutService();
+            var layoutHashId = "EOEb";
+
+            // Act
+            var response = await layoutService.GetErgodoxLayout(layoutHashId);
+
+            // Assert
+            Assert.IsType<ErgodoxLayout>(response);
+            Assert.Single(response.Revisions);
+            Assert.False(string.IsNullOrWhiteSpace(response.HashId));
+            Assert.False(string.IsNullOrWhiteSpace(response.Title));
+        }
+    }
+}
