4f6904f7fe8a3f757fab58f25e710c25776db3ff
Pierre C <invvard+github@outlook.com>
2019-01-25 15:55:17 -0500

    Add the ShowModalDialog signature

--- InvvardDev.EZLayoutDisplay.Desktop/Model/Service/Interface/IWindowService.cs
+++ InvvardDev.EZLayoutDisplay.Desktop/Model/Service/Interface/IWindowService.cs
@@ -4,9 +4,22 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.Model.Service.Interface
 {
     public interface IWindowService
     {
+        /// <summary>
+        /// Shows the specified <see cref="Window"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the <see cref="Window"/>.</typeparam>
         void ShowWindow<T>()
             where T : Window, new();
 
+        /// <summary>
+        /// Closes the specified <see cref="Window"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the <see cref="Window"/>.</typeparam>
         void CloseWindow<T>();
+
+        /// <summary>
+        /// Shows a modal window.
+        /// </summary>
+        void ShowModalDialog();
     }
 }
