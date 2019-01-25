cf4d49507610d70967271d1db0ee33956116d629
Pierre C <invvard+github@outlook.com>
2019-01-24 22:42:17 -0500

    Add models to the EZLayout support.

---
+++ InvvardDev.EZLayoutDisplay.Desktop/Model/Dictionary/KeyDefinitionDictionary.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using InvvardDev.EZLayoutDisplay.Desktop.Properties;
+using Newtonsoft.Json;
+
+namespace InvvardDev.EZLayoutDisplay.Desktop.Model.Dictionary
+{
+    public class KeyDefinitionDictionary
+    {
+        public List<KeyDefinition> KeyDefinitions { get; private set; }
+
+        public KeyDefinitionDictionary()
+        {
+            InitializeKeyDefinitions();
+        }
+
+        private void InitializeKeyDefinitions()
+        {
+            KeyDefinitions = new List<KeyDefinition>();
+
+            if (Resources.keyDefinitions.Length <= 0)
+            {
+                // TODO : add logging
+                return;
+            }
+
+            var json = Encoding.Default.GetString(Resources.keyDefinitions);
+
+            KeyDefinitions.AddRange(JsonConvert.DeserializeObject<List<KeyDefinition>>(json));
+        }
+    }
+}
