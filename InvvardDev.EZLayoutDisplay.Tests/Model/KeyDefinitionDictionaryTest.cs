6cf69ca526884c3128531e601a01da414341fa94
Invvard <7305493+Invvard@users.noreply.github.com>
2019-01-24 22:44:37 -0500

    Add the unit tests for the KeyDefinitionDictionary initialization

---
+++ InvvardDev.EZLayoutDisplay.Tests/Model/KeyDefinitionDictionaryTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using InvvardDev.EZLayoutDisplay.Desktop.Model;
+using InvvardDev.EZLayoutDisplay.Desktop.Model.Dictionary;
+using InvvardDev.EZLayoutDisplay.Desktop.Model.Enum;
+using Xunit;
+
+namespace InvvardDev.EZLayoutDisplay.Tests.Model
+{
+    public class KeyDefinitionDictionaryTest
+    {
+        [ Fact ]
+        public void InitializeKeyDefinitions()
+        {
+            // Arrange
+
+            // Act
+            var dictionary = new KeyDefinitionDictionary();
+
+            // Assert
+            Assert.NotEmpty(dictionary.KeyDefinitions);
+            Assert.Equal(330, dictionary.KeyDefinitions.Count);
+        }
+    }
+}
