7635dac991426a0b28b6d0778f1e942899120c57
Pierre C <invvard+github@outlook.com>
2019-01-26 16:14:07 -0500

    Add EZLayout models

---
+++ InvvardDev.EZLayoutDisplay.Desktop/Model/EZKey.cs
@@ -0,0 +1,25 @@
+﻿namespace InvvardDev.EZLayoutDisplay.Desktop.Model
+{
+    public class EZKey
+    {
+        /// <summary>
+        /// Gets or sets the key position on layout.
+        /// </summary>
+        public int Position { get; set; }
+
+        /// <summary>
+        /// Gets or sets the key main label.
+        /// </summary>
+        public string Label { get; set; }
+
+        /// <summary>
+        /// Gets or sets the key secondary label.
+        /// </summary>
+        public string SubLabel { get; set; }
+
+        /// <summary>
+        /// Gets or sets the key glowing color.
+        /// </summary>
+        public string Color { get; set; }
+    }
+}
