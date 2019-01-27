a4750943e556d46776d9be0d36cd2874268929ec
Invvard <7305493+Invvard@users.noreply.github.com>
2019-01-26 23:45:09 -0500

    Add EZLayoutMaker tests

---
+++ InvvardDev.EZLayoutDisplay.Tests/Helper/EZLayoutMakerTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using InvvardDev.EZLayoutDisplay.Desktop.Helper;
+using InvvardDev.EZLayoutDisplay.Desktop.Model;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace InvvardDev.EZLayoutDisplay.Tests.Helper
+{
+    public class EZLayoutMakerTest
+    {
+        [ Theory ]
+        [ InlineData("expectedTitle", "expectedHashId") ]
+        public void PrepareEZLayout_InitializeEZLayout(string expectedTitle, string expectedHashId)
+        {
+            // Arrange
+            var revision = new Revision {
+                                            Layers = new List<ErgodoxLayer>()
+                                        };
+            ErgodoxLayout ergodoxLayout = new ErgodoxLayout {
+                                                                Title = expectedTitle,
+                                                                HashId = expectedHashId,
+                                                                Revisions = new List<Revision>()
+                                                            };
+            ergodoxLayout.Revisions.Add(revision);
+            EZLayout ezLayoutResult;
+
+            // Act
+            var ezLayoutMaker = new EZLayoutMaker();
+            ezLayoutResult = ezLayoutMaker.PrepareEZLayout(ergodoxLayout);
+
+            // Assert
+            Assert.Equal(expectedTitle, ezLayoutResult.Name);
+            Assert.Equal(expectedHashId, ezLayoutResult.HashId);
+        }
+    }
+}
