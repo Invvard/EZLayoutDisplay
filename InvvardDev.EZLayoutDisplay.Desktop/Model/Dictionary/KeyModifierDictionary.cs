e78e390de71500d189b8b7135d2310f2cffd8dc4
Pierre C <invvard+github@outlook.com>
2019-01-29 22:31:50 -0500

    Invert index position for Win & Shift modifiers

--- InvvardDev.EZLayoutDisplay.Desktop/Model/Dictionary/KeyModifierDictionary.cs
+++ InvvardDev.EZLayoutDisplay.Desktop/Model/Dictionary/KeyModifierDictionary.cs
@@ -17,12 +17,12 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.Model.Dictionary
             EZModifiers = new List<EZModifier> {
                                                    new EZModifier(KeyModifier.LeftAlt, 0, "A", "ALT", "Alt"),
                                                    new EZModifier(KeyModifier.LeftCtrl, 1, "C", "CTL", "Ctrl"),
-                                                   new EZModifier(KeyModifier.LeftShift, 2, "S", "SFT", "Shift"),
-                                                   new EZModifier(KeyModifier.LeftWin, 3, "W", "WIN", "Win"),
+                                                   new EZModifier(KeyModifier.LeftWin, 2, "W", "WIN", "Win"),
+                                                   new EZModifier(KeyModifier.LeftShift, 3, "S", "SFT", "Shift"),
                                                    new EZModifier(KeyModifier.RightAlt, 4, "A", "ALT", "Alt"),
                                                    new EZModifier(KeyModifier.RightCtrl, 5, "C", "CTL", "Ctrl"),
-                                                   new EZModifier(KeyModifier.RightShift, 6, "S", "SFT", "Shift"),
-                                                   new EZModifier(KeyModifier.RightWin, 7, "W", "WIN", "Win")
+                                                   new EZModifier(KeyModifier.RightWin, 6, "W", "WIN", "Win"),
+                                                   new EZModifier(KeyModifier.RightShift, 7, "S", "SFT", "Shift")
                                                };
 
         }
