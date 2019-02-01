51065342f39763da8d53750f209f1dc668d23c32
Invvard <7305493+Invvard@users.noreply.github.com>
2019-01-31 22:56:11 -0500

    Add models and data sources

---
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Model/Dictionary/LayoutTemplateDictionary.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using InvvardDev.EZLayoutDisplay.Desktop.Properties;
+using Newtonsoft.Json;
+
+namespace InvvardDev.EZLayoutDisplay.Desktop.Model.Dictionary
+{
+    public class LayoutTemplateDictionary
+    {
+        public ObservableCollection<KeyTemplate> KeyBaseTemplates { get; private set; }
+
+        public LayoutTemplateDictionary()
+        {
+            InitializeLayoutTemplate();
+        }
+
+        private void InitializeLayoutTemplate()
+        {
+            KeyBaseTemplates = new ObservableCollection<KeyTemplate>();
+
+            if (Resources.layoutTemplate.Length <= 0)
+            {
+                // TODO : add logging
+                return;
+            }
+
+            var json = Encoding.Default.GetString(Resources.layoutTemplate);
+
+            KeyBaseTemplates = JsonConvert.DeserializeObject<ObservableCollection<KeyTemplate>>(json);
+        }
+    }
+}
