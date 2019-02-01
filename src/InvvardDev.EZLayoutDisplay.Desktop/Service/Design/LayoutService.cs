efbd8f01cc2db364313db20d02c4d1333f5f7f62
Pierre C <invvard+github@outlook.com>
2019-02-01 17:52:02 -0500

    Add the GetLayoutTemplate method

--- src/InvvardDev.EZLayoutDisplay.Desktop/Service/Design/LayoutService.cs
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Service/Design/LayoutService.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using InvvardDev.EZLayoutDisplay.Desktop.Model;
 using InvvardDev.EZLayoutDisplay.Desktop.Service.Interface;
@@ -21,6 +22,24 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.Service.Design
             Debug.WriteLine("Layout prepared");
 
             return new EZLayout();
+        }
+
+        public ObservableCollection<KeyTemplate> GetLayoutTemplate()
+        {
+            var layoutTemplate = new ObservableCollection<KeyTemplate> {
+                                                                           new KeyTemplate(0, 0, 1.5, vOffset: 0.37),
+                                                                           new KeyTemplate(1.5, 0, vOffset: 0.37, isGlowing: true),
+                                                                           new KeyTemplate(2.5, 0, vOffset: 129, isGlowing: true),
+                                                                           new KeyTemplate(3.5, 0, isGlowing: true),
+                                                                           new KeyTemplate(0, 1, 1.5, vOffset:.37, isGlowing: true),
+                                                                           new KeyTemplate(1.5, 1, vOffset:.37, isGlowing: true),
+                                                                           new KeyTemplate(2.5, 1, vOffset:.129, isGlowing: true),
+                                                                           new KeyTemplate(3.5, 1, isGlowing: true),
+                                                                       };
+
+
+
+            return layoutTemplate;
         }
     }
 }
--- src/InvvardDev.EZLayoutDisplay.Desktop/Service/Implementation/LayoutService.cs
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Service/Implementation/LayoutService.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using InvvardDev.EZLayoutDisplay.Desktop.Helper;
 using InvvardDev.EZLayoutDisplay.Desktop.Model;
+using InvvardDev.EZLayoutDisplay.Desktop.Properties;
 using InvvardDev.EZLayoutDisplay.Desktop.Service.Interface;
 using Newtonsoft.Json;
 
@@ -11,18 +13,21 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.Service.Implementation
 {
     public class LayoutService : ILayoutService
     {
-        private readonly string GetLayoutBody = "{{\"operationName\":\"getLayout\",\"variables\":{{\"hashId\":\"{0}\"}},\"query\":\"query getLayout($hashId: String!) {{\\n Layout(hashId: $hashId) {{\\n ...LayoutData\\n }}\\n}}\\n\\nfragment LayoutData on Layout {{\\n hashId\\n title\\n revisions {{\\n hashId\\n model\\n layers {{\\n hashId\\n keys\\n position\\n title\\n color\\n }}\\n}}\\n}}\\n\"}}";
+        private readonly string GetLayoutBody =
+            "{{\"operationName\":\"getLayout\",\"variables\":{{\"hashId\":\"{0}\"}},\"query\":\"query getLayout($hashId: String!) {{\\n Layout(hashId: $hashId) {{\\n ...LayoutData\\n }}\\n}}\\n\\nfragment LayoutData on Layout {{\\n hashId\\n title\\n revisions {{\\n hashId\\n model\\n layers {{\\n hashId\\n keys\\n position\\n title\\n color\\n }}\\n}}\\n}}\\n\"}}";
+
         private const string GetLayoutRequestUri = "https://oryx.ergodox-ez.com/graphql";
+        private ObservableCollection<KeyTemplate> _layoutTemplate;
+
+        #region ILayoutService implementation
 
         /// <inheritdoc />
         public async Task<ErgodoxLayout> GetErgodoxLayout(string layoutHashId)
         {
-            if (string.IsNullOrWhiteSpace(layoutHashId))
-            {
-                throw new ArgumentNullException(nameof(layoutHashId), "Layout hash ID was not found.");
-            }
+            if (string.IsNullOrWhiteSpace(layoutHashId)) { throw new ArgumentNullException(nameof(layoutHashId), "Layout hash ID was not found."); }
 
             DataRoot layout;
+
             using (HttpClient client = new HttpClient())
             {
                 var body = string.Format(GetLayoutBody, layoutHashId);
@@ -31,10 +36,7 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.Service.Implementation
 
                 layout = JsonConvert.DeserializeObject<DataRoot>(result);
 
-                if (layout?.LayoutRoot?.Layout == null)
-                {
-                    throw new ArgumentException(layoutHashId, $"Hash ID \"{layoutHashId}\" does not exist");
-                }
+                if (layout?.LayoutRoot?.Layout == null) { throw new ArgumentException(layoutHashId, $"Hash ID \"{layoutHashId}\" does not exist"); }
             }
 
             return layout.LayoutRoot.Layout;
@@ -48,5 +50,32 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.Service.Implementation
 
             return ezLayout;
         }
+
+        /// <inheritdoc />
+        public ObservableCollection<KeyTemplate> GetLayoutTemplate()
+        {
+            return _layoutTemplate ?? (_layoutTemplate = ReadLayoutDefinition());
+        }
+
+        #endregion
+
+        #region Private methods
+        
+        private ObservableCollection<KeyTemplate> ReadLayoutDefinition()
+        {
+            if (Resources.layoutDefinition.Length <= 0)
+            {
+                // TODO : add logging
+                return new ObservableCollection<KeyTemplate>();
+            }
+
+            var json = Encoding.Default.GetString(Resources.layoutDefinition);
+
+            var layoutDefinition = JsonConvert.DeserializeObject<ObservableCollection<KeyTemplate>>(json);
+
+            return layoutDefinition;
+        } 
+
+        #endregion
     }
 }
