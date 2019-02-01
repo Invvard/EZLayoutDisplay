0d8e39c14d780889f0b3195610018bbe885e4285
Pierre C <invvard+github@outlook.com>
2019-01-31 19:48:35 -0500

    Add closing behavior to the DisplayLayoutWindow

--- src/InvvardDev.EZLayoutDisplay.Desktop/ViewModel/DisplayLayoutViewModel.cs
+++ src/InvvardDev.EZLayoutDisplay.Desktop/ViewModel/DisplayLayoutViewModel.cs
@@ -1,9 +1,16 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using InvvardDev.EZLayoutDisplay.Desktop.Service.Interface;
+using InvvardDev.EZLayoutDisplay.Desktop.View;
 
 namespace InvvardDev.EZLayoutDisplay.Desktop.ViewModel
 {
     public class DisplayLayoutViewModel : ViewModelBase
     {
+        private IWindowService _windowService;
+        private ICommand _lostFocusCommand;
+        private ICommand _keyPressCommand;
         private string _windowTitle;
 
         public string WindowTitle
@@ -12,14 +19,32 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.ViewModel
             set => Set(ref _windowTitle, value);
         }
 
-        public DisplayLayoutViewModel()
+        #region Relay commands
+
+        /// <summary>
+        /// Lost focus command.
+        /// </summary>
+        public ICommand LostFocusCommand =>
+            _lostFocusCommand
+            ?? (_lostFocusCommand = new RelayCommand(LostFocus));
+
+        #endregion
+
+        public DisplayLayoutViewModel(IWindowService windowService)
         {
+            _windowService = windowService;
+
             SetLabelUi();
         }
 
         private void SetLabelUi()
         {
             WindowTitle = "Ergodox Layout";
+        }
+
+        private void LostFocus()
+        {
+            _windowService.CloseWindow<DisplayLayoutWindow>();
         }
     }
 }
