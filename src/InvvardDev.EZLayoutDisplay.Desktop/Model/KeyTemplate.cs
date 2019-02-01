ce278e20d2d44fb338872ab96a57748f27bf7f06
Pierre C <invvard+github@outlook.com>
2019-02-01 17:50:41 -0500

    Modify the KeyTemplate model

--- src/InvvardDev.EZLayoutDisplay.Desktop/Model/KeyTemplate.cs
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Model/KeyTemplate.cs
@@ -2,18 +2,44 @@
 {
     public class KeyTemplate
     {
-        public double BaseX { get; set; }
-        public double BaseY { get; set; }
-        public double TopX => BaseX + 3;
-        public double TopY => BaseY + 3;
-        public double BaseWidth { get; set; }
-        public double BaseHeight { get; set; }
-        public double TopWidth => BaseWidth - 6;
-        public double TopHeight => BaseHeight - 11;
+        private const int KeyUnitSize = 54;
+
+        public double BaseRelativeLeft { get; set; }
+        public double BaseRelativeTop { get; set; }
+        public double BaseRelativeWidth { get; set; }
+        public double BaseRelativeHeight { get; set; }
         public double VerticalOffset { get; set; }
         public double HorizontalOffset { get; set; }
         public int Rotate { get; set; }
         public bool IsTopRightRotationOrigin { get; set; }
         public bool IsGlowing { get; set; }
+
+        public double BaseKeyX => HorizontalOffset * KeyUnitSize + BaseRelativeLeft * KeyUnitSize;
+        public double BaseKeyY => VerticalOffset * KeyUnitSize + BaseRelativeTop * KeyUnitSize;
+        public double TopKeyX => BaseKeyX + 3;
+        public double TopKeyY => BaseKeyY + 3;
+
+        public double BaseKeyWidth => BaseRelativeWidth * KeyUnitSize;
+        public double BaseKeyHeight => BaseRelativeHeight * KeyUnitSize;
+        public double TopKeyWidth => BaseKeyWidth - 6;
+        public double TopKeyHeight => BaseKeyHeight - 11;
+
+        public KeyTemplate()
+        {
+            
+        }
+
+        public KeyTemplate(double x, double y, double width = 1, double height = 1, double vOffset = 0, double hOffset = 0, int rotate = 0, bool isOriginTopRight = false, bool isGlowing = false)
+        {
+            BaseRelativeLeft = x;
+            BaseRelativeTop = y;
+            BaseRelativeWidth = width;
+            BaseRelativeHeight = height;
+            VerticalOffset = vOffset;
+            HorizontalOffset = hOffset;
+            Rotate = rotate;
+            IsTopRightRotationOrigin = isOriginTopRight;
+            IsGlowing = isGlowing;
+        }
     }
 }
