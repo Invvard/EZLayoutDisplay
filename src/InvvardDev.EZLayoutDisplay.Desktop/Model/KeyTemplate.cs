12f74cd6fbe98977fdcf95eda59ba1d19f7eb5b7
Invvard <7305493+Invvard@users.noreply.github.com>
2019-02-02 17:55:35 -0500

    Add JSON property attribute

--- src/InvvardDev.EZLayoutDisplay.Desktop/Model/KeyTemplate.cs
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Model/KeyTemplate.cs
@@ -1,17 +1,71 @@
-﻿namespace InvvardDev.EZLayoutDisplay.Desktop.Model
+﻿using System.ComponentModel;
+using Newtonsoft.Json;
+
+namespace InvvardDev.EZLayoutDisplay.Desktop.Model
 {
     public class KeyTemplate
     {
         private const int KeyUnitSize = 54;
 
+        /// <summary>
+        /// Gets or sets the key X position.
+        /// </summary>
+        [JsonProperty("x")]
         public double BaseRelativeLeft { get; set; }
+
+        /// <summary>
+        /// Gets or sets the key Y position.
+        /// </summary>
+        [JsonProperty("y")]
         public double BaseRelativeTop { get; set; }
+
+        /// <summary>
+        /// Gets or sets the key .
+        /// </summary>
+        [JsonProperty("width", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        [DefaultValue(1)]
         public double BaseRelativeWidth { get; set; }
+
+        /// <summary>
+        /// Gets or sets the key .
+        /// </summary>
+        [JsonProperty("height", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        [DefaultValue(1)]
         public double BaseRelativeHeight { get; set; }
+
+        /// <summary>
+        /// Gets or sets the key .
+        /// </summary>
+        [JsonProperty("voffset", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        [DefaultValue(0)]
         public double VerticalOffset { get; set; }
+
+        /// <summary>
+        /// Gets or sets the key .
+        /// </summary>
+        [JsonProperty("hoffset", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        [DefaultValue(0)]
         public double HorizontalOffset { get; set; }
+
+        /// <summary>
+        /// Gets or sets the key .
+        /// </summary>
+        [JsonProperty("rotate", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        [DefaultValue(0)]
         public int Rotate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the key .
+        /// </summary>
+        //[JsonProperty("origin", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        [DefaultValue(false)]
         public bool IsTopRightRotationOrigin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the key .
+        /// </summary>
+        //[JsonProperty("glow", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        [DefaultValue(false)]
         public bool IsGlowing { get; set; }
 
         public double BaseKeyX => HorizontalOffset * KeyUnitSize + BaseRelativeLeft * KeyUnitSize;
