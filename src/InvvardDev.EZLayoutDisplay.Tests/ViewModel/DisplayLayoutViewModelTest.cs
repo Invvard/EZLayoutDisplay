8ade617f1c2d2b364fc5407cb040dcd5518773e9
Pierre C <invvard+github@outlook.com>
2019-02-03 22:35:45 -0500

    Add a test on PopulateModel

--- src/InvvardDev.EZLayoutDisplay.Tests/ViewModel/DisplayLayoutViewModelTest.cs
+++ src/InvvardDev.EZLayoutDisplay.Tests/ViewModel/DisplayLayoutViewModelTest.cs
@@ -1,4 +1,6 @@
-﻿using InvvardDev.EZLayoutDisplay.Desktop.Service.Interface;
+﻿using System.Collections.Generic;
+using InvvardDev.EZLayoutDisplay.Desktop.Model;
+using InvvardDev.EZLayoutDisplay.Desktop.Service.Interface;
 using InvvardDev.EZLayoutDisplay.Desktop.View;
 using InvvardDev.EZLayoutDisplay.Desktop.ViewModel;
 using Moq;
@@ -13,9 +15,10 @@ namespace InvvardDev.EZLayoutDisplay.Tests.ViewModel
         {
             //Arrange
             var mockWindowService = new Mock<IWindowService>();
+            var mockLayoutService = new Mock<ILayoutService>();
 
             //Act
-            var displayLayoutViewModel = new DisplayLayoutViewModel(mockWindowService.Object);
+            var displayLayoutViewModel = new DisplayLayoutViewModel(mockWindowService.Object, mockLayoutService.Object);
 
             //Assert
             Assert.Equal("Ergodox Layout", displayLayoutViewModel.WindowTitle);
@@ -27,13 +30,37 @@ namespace InvvardDev.EZLayoutDisplay.Tests.ViewModel
             //Arrange
             var mockWindowService = new Mock<IWindowService>();
             mockWindowService.Setup(w => w.CloseWindow<DisplayLayoutWindow>()).Verifiable();
+            var mockLayoutService = new Mock<ILayoutService>();
 
             //Act
-            var displayLayoutViewModel = new DisplayLayoutViewModel(mockWindowService.Object);
+            var displayLayoutViewModel = new DisplayLayoutViewModel(mockWindowService.Object, mockLayoutService.Object);
             displayLayoutViewModel.LostFocusCommand.Execute(null);
 
             //Assert
             mockWindowService.Verify(w => w.CloseWindow<DisplayLayoutWindow>(), Times.Once);
+        }
+
+        [ Theory ]
+        [ InlineData(0) ]
+        [ InlineData(1) ]
+        [ InlineData(76) ]
+        public void PopulateModel(int numberOfKey)
+        {
+            //Arrange
+            var layoutTemplate = new List<KeyTemplate>();
+
+            for (int i = 0 ; i < numberOfKey ; i++) { layoutTemplate.Add(new KeyTemplate(i, i, 54, 81)); }
+
+            var mockWindowService = new Mock<IWindowService>();
+            var mockLayoutService = new Mock<ILayoutService>();
+            mockLayoutService.Setup(l => l.GetLayoutTemplate()).ReturnsAsync(layoutTemplate).Verifiable();
+
+            //Act
+            var displayLayoutViewModel = new DisplayLayoutViewModel(mockWindowService.Object, mockLayoutService.Object);
+
+            //Assert
+            mockLayoutService.Verify(l => l.GetLayoutTemplate(), Times.Once);
+            Assert.Equal(numberOfKey, displayLayoutViewModel.LayoutTemplate.Count);
         }
     }
 }
