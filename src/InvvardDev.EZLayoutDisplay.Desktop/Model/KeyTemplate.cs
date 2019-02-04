bc2a1263cb8861e9e17ad3a8cb69bca50cc8f71d
Invvard <7305493+Invvard@users.noreply.github.com>
2019-02-03 22:08:22 -0500

    Update the Layout Definition model and datasource

--- src/InvvardDev.EZLayoutDisplay.Desktop/Model/KeyTemplate.cs
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Model/KeyTemplate.cs
@@ -20,51 +20,51 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.Model
         public double BaseRelativeTop { get; set; }
 
         /// <summary>
-        /// Gets or sets the key .
+        /// Gets or sets the key width.
         /// </summary>
         [JsonProperty("width", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
         [DefaultValue(1)]
         public double BaseRelativeWidth { get; set; }
 
         /// <summary>
-        /// Gets or sets the key .
+        /// Gets or sets the key height.
         /// </summary>
         [JsonProperty("height", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
         [DefaultValue(1)]
         public double BaseRelativeHeight { get; set; }
 
         /// <summary>
-        /// Gets or sets the key .
+        /// Gets or sets the key vertical offset.
         /// </summary>
         [JsonProperty("voffset", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
         [DefaultValue(0)]
         public double VerticalOffset { get; set; }
 
         /// <summary>
-        /// Gets or sets the key .
+        /// Gets or sets the key horizontal offset.
         /// </summary>
         [JsonProperty("hoffset", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
         [DefaultValue(0)]
         public double HorizontalOffset { get; set; }
 
         /// <summary>
-        /// Gets or sets the key .
+        /// Gets or sets the key rotation angle.
         /// </summary>
         [JsonProperty("rotate", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
         [DefaultValue(0)]
-        public int Rotate { get; set; }
+        public int RotationAngle { get; set; }
+
+        /// <summary>
+        /// Gets or sets the key rotation origin.
+        /// </summary>
+        [JsonProperty("origin", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        [DefaultValue("0,0")]
+        public string RotationOrigin { get; set; }
 
         /// <summary>
         /// Gets or sets the key .
         /// </summary>
-        //[JsonProperty("origin", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
-        [DefaultValue(false)]
-        public bool IsTopRightRotationOrigin { get; set; }
-
-        /// <summary>
-        /// Gets or sets the key .
-        /// </summary>
-        //[JsonProperty("glow", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        [JsonProperty("glow", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
         [DefaultValue(false)]
         public bool IsGlowing { get; set; }
 
@@ -83,7 +83,7 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.Model
             
         }
 
-        public KeyTemplate(double x, double y, double width = 1, double height = 1, double vOffset = 0, double hOffset = 0, int rotate = 0, bool isOriginTopRight = false, bool isGlowing = false)
+        public KeyTemplate(double x, double y, double width = 1, double height = 1, double vOffset = 0, double hOffset = 0, int rotationAngle = 0, string rotationOrigin = "0,0", bool isGlowing = false)
         {
             BaseRelativeLeft = x;
             BaseRelativeTop = y;
@@ -91,8 +91,8 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.Model
             BaseRelativeHeight = height;
             VerticalOffset = vOffset;
             HorizontalOffset = hOffset;
-            Rotate = rotate;
-            IsTopRightRotationOrigin = isOriginTopRight;
+            RotationAngle = rotationAngle;
+            RotationOrigin = rotationOrigin;
             IsGlowing = isGlowing;
         }
     }
