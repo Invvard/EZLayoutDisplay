3c75b38065e432a7daaaee6a398ec751bf58f37f
Invvard <7305493+Invvard@users.noreply.github.com>
2019-02-05 18:41:13 -0500

    Add the KeyDisplayType

---
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Model/Enum/KeyDisplayType.cs
@@ -0,0 +1,35 @@
+﻿namespace InvvardDev.EZLayoutDisplay.Desktop.Model.Enum
+{
+    public enum KeyDisplayType
+    {
+        /// <summary>
+        /// Nothing to display (KC_TRANSPARENT).
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Display the main label.
+        /// </summary>
+        SimpleLabel = 1,
+
+        /// <summary>
+        /// Display a label with the international indicator at the top right.
+        /// </summary>
+        LabelInternational = 2,
+
+        /// <summary>
+        /// Display a label with a sub label under.
+        /// </summary>
+        LabelWithSubLabelUnder = 3,
+
+        /// <summary>
+        /// Display a label with a sub label on top.
+        /// </summary>
+        LabelWithSubLabelOnTop = 4,
+
+        /// <summary>
+        /// Display a glyph and no label.
+        /// </summary>
+        Glyph = 5,
+    }
+}
