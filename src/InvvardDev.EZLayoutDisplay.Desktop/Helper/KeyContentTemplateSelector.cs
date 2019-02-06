dbaa25fc35247f976f2fb521d08625dff4f05a10
Pierre C <invvard+github@outlook.com>
2019-02-05 22:52:05 -0500

    Add the KeyContentTemplateSelector

---
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Helper/KeyContentTemplateSelector.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using InvvardDev.EZLayoutDisplay.Desktop.Model;
+using InvvardDev.EZLayoutDisplay.Desktop.Model.Enum;
+
+namespace InvvardDev.EZLayoutDisplay.Desktop.Helper
+{
+    public class KeyContentTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate SimpleLabelDataTemplate { get; set; }
+        public DataTemplate GlyphDataTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var key = item as KeyTemplate;
+
+            DataTemplate template = SimpleLabelDataTemplate;
+
+            switch (key.EZKey.DisplayType)
+            {
+                case KeyDisplayType.None:
+                case KeyDisplayType.SimpleLabel:
+                    template = SimpleLabelDataTemplate;
+                    break;
+                case KeyDisplayType.LabelInternational:
+
+                    break;
+                case KeyDisplayType.LabelWithSubLabelUnder:
+
+                    break;
+                case KeyDisplayType.LabelWithSubLabelOnTop:
+
+                    break;
+                case KeyDisplayType.Glyph:
+                    template = GlyphDataTemplate;
+                    break;
+            }
+
+            return template;
+        }
+    }
+}
