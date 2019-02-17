7dde41a1015680434a8c43d6fe0ba85f713e09da
Pierre C <invvard+github@outlook.com>
2019-02-17 05:38:13 -0500

    Fix code and unit tests

--- src/InvvardDev.EZLayoutDisplay.Tests/ViewModel/DisplayLayoutViewModelTest.cs
+++ src/InvvardDev.EZLayoutDisplay.Tests/ViewModel/DisplayLayoutViewModelTest.cs
@@ -56,43 +56,96 @@ namespace InvvardDev.EZLayoutDisplay.Tests.ViewModel
         }
 
         [ Theory ]
-        [ InlineData(0) ]
-        [ InlineData(1) ]
-        [ InlineData(76) ]
-        public void PopulateModel(int numberOfKey)
+        [ InlineData(0, 1) ]
+        [ InlineData(1, 2) ]
+        [ InlineData(76, 2) ]
+        public void LoadCompleteLayout(int numberOfKey, int numberOfLayer)
         {
             //Arrange
             var layoutTemplate = new List<KeyTemplate>();
+            var ezKeys = new List<EZKey>();
 
             for (int i = 0 ; i < numberOfKey ; i++)
             {
                 layoutTemplate.Add(new KeyTemplate(i, i, 54, 81));
+                ezKeys.Add(new EZKey());
+            }
+
+            var keyboardLayout = new EZLayout();
+
+            for (int i = 0 ; i < numberOfLayer; i++)
+            {
+                keyboardLayout.EZLayers.Add(new EZLayer {
+                                                            Index = i,
+                                                            EZKeys = new List<EZKey>(ezKeys)
+                                                        });
             }
 
             var mockWindowService = new Mock<IWindowService>();
             var mockLayoutService = new Mock<ILayoutService>();
             mockLayoutService.Setup(l => l.GetLayoutTemplate()).ReturnsAsync(layoutTemplate).Verifiable();
             var mockSettingsService = new Mock<ISettingsService>();
+            mockSettingsService.SetupProperty(s => s.EZLayout, keyboardLayout);
 
             //Act
             var displayLayoutViewModel = new DisplayLayoutViewModel(mockWindowService.Object, mockLayoutService.Object, mockSettingsService.Object);
 
             //Assert
-            mockLayoutService.Verify(l => l.GetLayoutTemplate(), Times.Once);
-            Assert.Equal(numberOfKey, displayLayoutViewModel.LayoutTemplate.Count);
+            mockLayoutService.Verify(l => l.GetLayoutTemplate(), Times.AtMost(numberOfLayer));
+            Assert.Equal(numberOfKey, displayLayoutViewModel.CurrentLayoutTemplate.Count);
         }
 
         [ Theory ]
-        [ InlineData(1, 0, 0) ]
-        [ InlineData(1, 1, 0) ]
-        [ InlineData(2, 0, 0) ]
-        [ InlineData(2, 1, 1) ]
-        [ InlineData(2, 2, 0) ]
-        [ InlineData(3, 0, 0) ]
-        [ InlineData(3, 1, 1) ]
-        [ InlineData(3, 2, 2) ]
-        [ InlineData(3, 3, 0) ]
-        public void NextLayerCommand(int layerNumber, int nextLayerHit, int expectedCurrentLayerIndex)
+        [ InlineData(0, false) ]
+        [ InlineData(1, true) ]
+        public void NextLayerCommand_CanExecute(int layerNumber, bool expectedCanExecute)
+        {
+            //Arrange
+            var keyboardLayout = new EZLayout();
+
+            for (int i = 0; i < layerNumber; i++)
+            {
+                keyboardLayout.EZLayers.Add(new EZLayer
+                                            {
+                                                Index = i,
+                                                EZKeys = new List<EZKey> {
+                                                                             new EZKey()
+                                                                         }
+                                            });
+            }
+
+            var layoutTemplate = new List<KeyTemplate>();
+
+            for (int i = 0; i < 1; i++)
+            {
+                layoutTemplate.Add(new KeyTemplate(i, i, 54, 81));
+            }
+
+            var mockLayoutService = new Mock<ILayoutService>();
+            mockLayoutService.Setup(l => l.GetLayoutTemplate()).ReturnsAsync(layoutTemplate);
+            var mockWindowService = new Mock<IWindowService>();
+            var mockSettingsService = new Mock<ISettingsService>();
+            mockSettingsService.SetupProperty(s => s.EZLayout, keyboardLayout);
+
+            //Act
+            var displayLayoutViewModel = new DisplayLayoutViewModel(mockWindowService.Object, mockLayoutService.Object, mockSettingsService.Object);
+            
+            //Assert
+            Assert.Equal(expectedCanExecute, displayLayoutViewModel.NextLayerCommand.CanExecute(null));
+        }
+
+        [ Theory ]
+        [ InlineData(0, 0, 0, false) ]
+        [ InlineData(1, 0, 0, true) ]
+        [ InlineData(1, 1, 0, true) ]
+        [ InlineData(2, 0, 0, true) ]
+        [ InlineData(2, 1, 1, true) ]
+        [ InlineData(2, 2, 0, true) ]
+        [ InlineData(3, 0, 0, true) ]
+        [ InlineData(3, 1, 1, true) ]
+        [ InlineData(3, 2, 2, true) ]
+        [ InlineData(3, 3, 0, true) ]
+        public void NextLayerCommand_Execute(int layerNumber, int nextLayerHit, int expectedCurrentLayerIndex, bool expectedCanExecute)
         {
             //Arrange
             var keyboardLayout = new EZLayout();
@@ -107,10 +160,18 @@ namespace InvvardDev.EZLayoutDisplay.Tests.ViewModel
                                                         });
             }
 
+            var layoutTemplate = new List<KeyTemplate>();
+
+            for (int i = 0 ; i < 1 ; i++)
+            {
+                layoutTemplate.Add(new KeyTemplate(i, i, 54, 81));
+            }
+
             var mockLayoutService = new Mock<ILayoutService>();
+            mockLayoutService.Setup(l => l.GetLayoutTemplate()).ReturnsAsync(layoutTemplate);
             var mockWindowService = new Mock<IWindowService>();
             var mockSettingsService = new Mock<ISettingsService>();
-            mockSettingsService.Setup(s => s.EZLayout).Returns(keyboardLayout);
+            mockSettingsService.SetupProperty(s => s.EZLayout, keyboardLayout);
 
             //Act
             var displayLayoutViewModel = new DisplayLayoutViewModel(mockWindowService.Object, mockLayoutService.Object, mockSettingsService.Object);
