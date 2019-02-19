c63afed1d32b71c80973a0bd2c73adce03b97ff5
Invvard <7305493+Invvard@users.noreply.github.com>
2019-02-18 22:03:32 -0500

    Add the window title

--- src/InvvardDev.EZLayoutDisplay.Desktop/ViewModel/AboutViewModel.cs
+++ src/InvvardDev.EZLayoutDisplay.Desktop/ViewModel/AboutViewModel.cs
@@ -4,6 +4,45 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.ViewModel
 {
     public class AboutViewModel : ViewModelBase
     {
-        
+        #region Fields
+
+        private string _windowTitle;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the window title.
+        /// </summary>
+        public string WindowTitle
+        {
+            get => _windowTitle;
+            set => Set(ref _windowTitle, value);
+        }
+
+        #endregion
+
+        #region Relay commands
+
+        #endregion
+
+        #region Constructor
+
+        public AboutViewModel()
+        {
+            SetLabelUi();
+        }
+
+        #endregion
+
+        #region Private methods
+
+        private void SetLabelUi()
+        {
+            WindowTitle = "About EZ Layout Display";
+        }
+
+        #endregion
     }
 }
