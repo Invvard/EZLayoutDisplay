1ec7c325d1d11814211b150794725ad1ab075f5a
Invvard <7305493+Invvard@users.noreply.github.com>
2019-02-28 23:00:03 -0500

    Change property accessibility

--- src/InvvardDev.EZLayoutDisplay.Console/KeyDefinitionProcessor.cs
+++ src/InvvardDev.EZLayoutDisplay.Console/KeyDefinitionProcessor.cs
@@ -1,4 +1,11 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using InvvardDev.EZLayoutDisplay.Desktop.Model;
+using InvvardDev.EZLayoutDisplay.Desktop.Model.Enum;
+using Newtonsoft.Json;
 
 namespace InvvardDev.EZLayoutDisplay.Console
 {
@@ -8,14 +15,21 @@ namespace InvvardDev.EZLayoutDisplay.Console
 
         public KeyDefinitionProcessor() { }
 
-        public void RunProcess()
+        public async void RunProcess()
         {
             if (!CheckKeyDefinitionJsExists())
             {
                 return;
             }
 
-            ParseJson();
+            var keyCodesOrigin = ParseJson();
+
+            if (!keyCodesOrigin.Any())
+            {
+                return;
+            }
+
+            List<KeyDefinition> keyDefinitions = ToEZKeyDefinitions(keyCodesOrigin);
         }
 
         private bool CheckKeyDefinitionJsExists()
@@ -25,8 +39,51 @@ namespace InvvardDev.EZLayoutDisplay.Console
             return fileExist;
         }
 
-        private void ParseJson()
+        private List<ConfiguratorKeyDefinition> ParseJson()
         {
+            List<ConfiguratorKeyDefinition> keyCodes;
+
+            using (StreamReader sr = new StreamReader(KeyDefinitionJsFilename))
+            {
+                var json = sr.ReadToEnd();
+                keyCodes = JsonConvert.DeserializeObject<List<ConfiguratorKeyDefinition>>(json);
+            }
+
+            return keyCodes;
+        }
+
+        private List<KeyDefinition> ToEZKeyDefinitions(List<ConfiguratorKeyDefinition> keyCodesOrigin)
+        {
+            List<KeyDefinition> keyDefinitions = new List<KeyDefinition>();
+
+            foreach (var configKeyDef in keyCodesOrigin)
+            {
+                var keyDefinition = new KeyDefinition {
+                                                          Label = ChooseLabel(configKeyDef),
+                                                          Description = configKeyDef.Description,
+                                                          IsGlyph = string.IsNullOrWhiteSpace(configKeyDef.Glyph),
+                                                          KeyCode = configKeyDef.KeyCode,
+                                                          KeyCategory = (KeyCategory) Enum.Parse(typeof(KeyCategory), configKeyDef.Category, true),
+                                                          SecondaryCommand = configKeyDef.Command
+                                                      };
+
+                if (configKeyDef.PrecedingKey.HasValue)
+                {
+                    keyDefinition.PrecedingKey = configKeyDef.PrecedingKey.Value;
+                }
+
+                keyDefinitions.Add(keyDefinition);
+            }
+
+            return keyDefinitions;
+        }
+
+        private string ChooseLabel(ConfiguratorKeyDefinition configKeyDef)
+        {
+            if (string.IsNullOrWhiteSpace(configKeyDef.Label))
+            {
+                
+            }
         }
     }
 }
