2fb156a58bdc7299d48b763b25412c7d2519954c
Pierre C <invvard+github@outlook.com>
2019-02-28 23:03:48 -0500

    Remove list of OsDefinition non longer needed

--- src/InvvardDev.EZLayoutDisplay.Console/ConfiguratorKeyDefinition.cs
+++ src/InvvardDev.EZLayoutDisplay.Console/ConfiguratorKeyDefinition.cs
@@ -6,13 +6,12 @@ namespace InvvardDev.EZLayoutDisplay.Console
     {
         public string KeyCode { get; set; }
         public string Label { get; set; }
+        public object MenuLabel { get; set; }
+        public string AltLabel { get; set; }
+        public string Glyph { get; set; }
         public string Category { get; set; }
         public string Description { get; set; }
         public string Command { get; set; }
-        public object MenuLabel { get; set; }
-        public string AltLabel { get; set; }
         public bool? PrecedingKey { get; set; }
-        public string Glyph { get; set; }
-        public List<OsDefinition> OsDefinitions { get; set; }
     }
 }
