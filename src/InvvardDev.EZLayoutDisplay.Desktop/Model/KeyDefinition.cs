e6361a1ebaec2c4c3316f52c927f4fe04cb93dbf
Pierre C <invvard+github@outlook.com>
2019-03-02 12:16:31 -0500

    Add serialization attributes to the KeyDefinition model

--- src/InvvardDev.EZLayoutDisplay.Desktop/Model/KeyDefinition.cs
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Model/KeyDefinition.cs
@@ -1,5 +1,6 @@
 ﻿using InvvardDev.EZLayoutDisplay.Desktop.Model.Enum;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace InvvardDev.EZLayoutDisplay.Desktop.Model
 {
@@ -23,30 +24,31 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.Model
         /// Gets the key category.
         /// </summary>
         [JsonProperty("category")]
+        [JsonConverter(typeof(StringEnumConverter))]
         public KeyCategory KeyCategory { get; set; }
 
         /// <summary>
         /// Gets the key description.
         /// </summary>
-        [JsonProperty("description")]
+        [JsonProperty("description", NullValueHandling = NullValueHandling.Ignore)]
         public string Description { get; set; }
 
         /// <summary>
         /// Gets the key secondary command.
         /// </summary>
-        [JsonProperty("command")]
+        [JsonProperty("command", NullValueHandling = NullValueHandling.Ignore)]
         public string SecondaryCommand { get; set; }
 
         /// <summary>
         /// Gets the key glyph name to display.
         /// </summary>
-        [JsonProperty("isglyph")]
+        [JsonProperty("isglyph", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public bool IsGlyph { get; set; }
 
         /// <summary>
         /// Gets the key preceding key indicator (blocks the targeted layer).
         /// </summary>
-        [JsonProperty("precedingKey")]
+        [JsonProperty("precedingKey", DefaultValueHandling = DefaultValueHandling.Ignore)]
         public bool PrecedingKey { get; set; }
 
         #endregion
