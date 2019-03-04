0a349b096cd43b75ec773dc1a95996f40860417f
Pierre C <invvard+github@outlook.com>
2019-03-03 21:44:14 -0500

    Add the KeyCategoryToBrushConverter

---
+++ src/InvvardDev.EZLayoutDisplay.Desktop/View/Converter/KeyCategoryToBrushConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace InvvardDev.EZLayoutDisplay.Desktop.View.Converter
+{
+    public class KeyCategoryToBrushConverter : IValueConverter
+    {
+        public TYPE Type { get; set; }
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
