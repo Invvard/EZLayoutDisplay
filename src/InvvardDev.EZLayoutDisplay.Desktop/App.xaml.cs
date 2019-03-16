80e719ad3e304f34798146c073cfb36fb399914a
Invvard <7305493+Invvard@users.noreply.github.com>
2019-03-15 23:38:27 -0400

    Add the DispatcherUnhandledException

--- src/InvvardDev.EZLayoutDisplay.Desktop/App.xaml.cs
+++ src/InvvardDev.EZLayoutDisplay.Desktop/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using GalaSoft.MvvmLight.Threading;
+﻿using System.Windows.Threading;
+using GalaSoft.MvvmLight.Threading;
+using NLog;
 
 namespace InvvardDev.EZLayoutDisplay.Desktop
 {
@@ -7,9 +9,20 @@ namespace InvvardDev.EZLayoutDisplay.Desktop
     /// </summary>
     public partial class App
     {
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+
         static App()
         {
             DispatcherHelper.Initialize();
+        }
+
+        void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            // Process unhandled exception
+            _logger.Error(e.Exception, "Unhandled exception");
+
+            // Prevent default unhandled exception processing
+            e.Handled = true;
         }
     }
 }
