56366a1c50eb30af0e08e12a4ea73fd9fd1da87b
Invvard <7305493+Invvard@users.noreply.github.com>
2019-03-18 20:41:57 -0400

    Create LoggerConfiguration

---
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Helper/LoggerConfguration.cs
@@ -0,0 +1,40 @@
+﻿using NLog;
+
+namespace InvvardDev.EZLayoutDisplay.Desktop.Helper
+{
+    public class LoggerConfguration
+    {
+        internal static LogLevel GetLogLevel(string value)
+        {
+            LogLevel level;
+
+            switch (value.ToLower())
+            {
+                case "debug":
+                    level = LogLevel.Debug;
+
+                    break;
+                case "trace":
+                    level = LogLevel.Trace;
+
+                    break;
+                default:
+                    level = LogLevel.Warn;
+
+                    break;
+            }
+
+            return level;
+        }
+
+        internal static void AdjustLogLevel(LogLevel logLevel)
+        {
+            var target = LogManager.Configuration.FindTargetByName("logfile");
+
+            if (target != null)
+            {
+                LogManager.Configuration.AddRule(logLevel, LogLevel.Fatal, target);
+            }
+        }
+    }
+}
