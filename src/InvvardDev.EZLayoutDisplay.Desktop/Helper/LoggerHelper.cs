17b0d137bf13660216b3812148259404cd7b9adf
Invvard <7305493+Invvard@users.noreply.github.com>
2019-03-18 22:23:58 -0400

    Rename LoggerHelper

--- src/InvvardDev.EZLayoutDisplay.Desktop/Helper/LoggerHelper.cs
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Helper/LoggerHelper.cs
@@ -1,9 +1,20 @@
-﻿using NLog;
+﻿using System.Runtime.CompilerServices;
+using NLog;
 
 namespace InvvardDev.EZLayoutDisplay.Desktop.Helper
 {
-    public class LoggerConfguration
+    public class LoggerHelper
     {
+        internal static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        internal static void TraceMethod(string                      message          = "Method {0} called (line {1})",
+                                         [ CallerMemberName ] string memberName       = "",
+                                         [ CallerFilePath ]   string sourceFilePath   = "",
+                                         [ CallerLineNumber ] int    sourceLineNumber = 0)
+        {
+            Logger.Trace(message, memberName, sourceLineNumber);
+        }
+
         internal static LogLevel GetLogLevel(string value)
         {
             LogLevel level;
