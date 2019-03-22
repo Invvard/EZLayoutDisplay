cd2a2eb2633fc0a926372c0a9d6281a2fc880c29
Invvard <7305493+Invvard@users.noreply.github.com>
2019-03-21 22:40:03 -0400

    Add DebugParam extensions

--- src/InvvardDev.EZLayoutDisplay.Desktop/Helper/LoggerHelper.cs
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Helper/LoggerHelper.cs
@@ -32,6 +32,33 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.Helper
             logger.Trace(message, memberName, sourceLineNumber);
         }
 
+        internal static void DebugInputParam(this Logger                 logger,
+                                             string                      parameterName,
+                                             object                      parameter,
+                                             string                      message    = "[Input {method}] {parameterName} = {@parameterValue})",
+                                             [ CallerMemberName ] string memberName = "")
+        {
+            DebugParam(logger, parameterName, parameter, message, memberName);
+        }
+
+        internal static void DebugOutputParam(this Logger                 logger,
+                                              string                      parameterName,
+                                              object                      parameter,
+                                              string                      message    = "[Output {method}] {parameterName} = {@parameterValue})",
+                                              [ CallerMemberName ] string memberName = "")
+        {
+            DebugParam(logger, parameterName, parameter, message, memberName);
+        }
+
+        private static void DebugParam(Logger logger,
+                                       string parameterName,
+                                       object parameter,
+                                       string message,
+                                       string memberName)
+        {
+            logger.Debug(message, memberName, parameterName, parameter);
+        }
+
         internal static LogLevel GetLogLevel(string value)
         {
             LogLevel level;
