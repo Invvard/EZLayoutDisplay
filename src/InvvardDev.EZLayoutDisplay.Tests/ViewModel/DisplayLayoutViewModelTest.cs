e7eb12350bc83ad886e05c4c8881bd935adc2a5c
Invvard <7305493+Invvard@users.noreply.github.com>
2019-03-30 17:22:17 -0400

    Simplify constructor test

--- src/InvvardDev.EZLayoutDisplay.Tests/ViewModel/DisplayLayoutViewModelTest.cs
+++ src/InvvardDev.EZLayoutDisplay.Tests/ViewModel/DisplayLayoutViewModelTest.cs
@@ -17,14 +17,10 @@ namespace InvvardDev.EZLayoutDisplay.Tests.ViewModel
             var mockWindowService = new Mock<IWindowService>();
             var mockLayoutService = new Mock<ILayoutService>();
             var mockSettingsService = new Mock<ISettingsService>();
-            var expectedLayerName = "Layer name";
-            var expectedLayerIndex = 0;
             mockSettingsService.SetupProperty(s => s.EZLayout,
                                               new EZLayout {
                                                                EZLayers = new List<EZLayer> {
                                                                                                 new EZLayer {
-                                                                                                                Name = expectedLayerName,
-                                                                                                                Index = expectedLayerIndex,
                                                                                                                 EZKeys = new List<EZKey> {
                                                                                                                                              new EZKey {
                                                                                                                                                            Label = new KeyLabel("A"),
