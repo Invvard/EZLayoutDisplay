6fd9b4095b829aee5270e3b4bbd0ae193bc39d6f
Invvard <7305493+Invvard@users.noreply.github.com>
2019-03-30 18:26:45 -0400

    Unit test AboutViewModel constructor

--- src/InvvardDev.EZLayoutDisplay.Tests/ViewModel/AboutViewModelTest.cs
+++ src/InvvardDev.EZLayoutDisplay.Tests/ViewModel/AboutViewModelTest.cs
@@ -1,7 +1,34 @@
-﻿namespace InvvardDev.EZLayoutDisplay.Tests.ViewModel
+﻿using InvvardDev.EZLayoutDisplay.Desktop.Service.Interface;
+using InvvardDev.EZLayoutDisplay.Desktop.ViewModel;
+using Moq;
+using Xunit;
+
+namespace InvvardDev.EZLayoutDisplay.Tests.ViewModel
 {
     public class AboutViewModelTest
     {
-        
+        [ Fact ]
+        public void AboutViewModelConstructor()
+        {
+            // Arrange
+            var mockWindowService = new Mock<IWindowService>();
+
+            // Act
+            var aboutViewModel = new AboutViewModel(mockWindowService.Object);
+
+            // Assert
+            Assert.Equal("About EZ Layout Display", aboutViewModel.WindowTitle);
+            Assert.Equal("EZ Layout Display", aboutViewModel.AppTitleLabel);
+            Assert.Equal("v.0.3.1.2002", aboutViewModel.AppVersionLabel);
+            Assert.Equal("Created by", aboutViewModel.CreatedTitleLabel);
+            Assert.Equal("Based on", aboutViewModel.BasedOnTitleLabel);
+            Assert.Equal("Project home", aboutViewModel.ProjectHomeTitleLabel);
+            Assert.Equal("Contact", aboutViewModel.ContactTitleLabel);
+            Assert.Equal("Pierre CAVAROC", aboutViewModel.CreatorInfoLabel);
+            Assert.Equal("ErgoDox EZ Configurator", aboutViewModel.BasedOnInfoLabel);
+            Assert.Equal("EZ Layout Display", aboutViewModel.ProjectHomeInfoLabel);
+            Assert.Equal("@Invvard", aboutViewModel.ContactInfoLabel);
+            Assert.Equal("OK", aboutViewModel.CloseButtonLabel);
+        }
     }
 }
