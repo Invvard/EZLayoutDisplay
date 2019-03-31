dbc3ba577b8c31b429b2bd6c1bfcc9d511992412
Invvard <7305493+Invvard@users.noreply.github.com>
2019-03-30 23:04:29 -0400

    Cover Relay commands

--- src/InvvardDev.EZLayoutDisplay.Tests/ViewModel/AboutViewModelTest.cs
+++ src/InvvardDev.EZLayoutDisplay.Tests/ViewModel/AboutViewModelTest.cs
@@ -13,9 +13,10 @@ namespace InvvardDev.EZLayoutDisplay.Tests.ViewModel
         {
             // Arrange
             var mockWindowService = new Mock<IWindowService>();
+            var mockProcessService = new Mock<IProcessService>();
 
             // Act
-            var aboutViewModel = new AboutViewModel(mockWindowService.Object);
+            var aboutViewModel = new AboutViewModel(mockWindowService.Object, mockProcessService.Object);
 
             // Assert
             Assert.Equal("About EZ Layout Display", aboutViewModel.WindowTitle);
@@ -33,18 +34,70 @@ namespace InvvardDev.EZLayoutDisplay.Tests.ViewModel
         }
 
         [ Fact ]
-        public void CloseAboutWindowExecute()
+        public void CloseAboutWindow_Execute()
         {
             // Arrange
             var mockWindowService = new Mock<IWindowService>();
             mockWindowService.Setup(w => w.CloseWindow<AboutWindow>()).Verifiable();
+            var mockProcessService = new Mock<IProcessService>();
 
             // Act
-            var aboutViewModel = new AboutViewModel(mockWindowService.Object);
+            var aboutViewModel = new AboutViewModel(mockWindowService.Object, mockProcessService.Object);
             aboutViewModel.CloseAboutCommand.Execute(null);
 
             // Assert
             mockWindowService.Verify(w => w.CloseWindow<AboutWindow>(), Times.Once);
+        }
+
+        [ Fact ]
+        public void NavigateBasedOnUrl_Execute()
+        {
+            // Arrange
+            var expectedUrl = "https://configure.ergodox-ez.com/layouts/default/latest/0";
+            var mockWindowService = new Mock<IWindowService>();
+            var mockProcessService = new Mock<IProcessService>();
+            mockProcessService.Setup(p => p.StartWebUrl(expectedUrl)).Verifiable();
+
+            // Act
+            var aboutViewModel = new AboutViewModel(mockWindowService.Object, mockProcessService.Object);
+            aboutViewModel.NavigateBasedOnUrlCommand.Execute(null);
+
+            // Assert
+            mockProcessService.Verify(p => p.StartWebUrl(expectedUrl));
+        }
+
+        [ Fact ]
+        public void NavigateProjectHomeUrl_Execute()
+        {
+            // Arrange
+            var expectedUrl = "https://github.com/Invvard/EZLayoutDisplay";
+            var mockWindowService = new Mock<IWindowService>();
+            var mockProcessService = new Mock<IProcessService>();
+            mockProcessService.Setup(p => p.StartWebUrl(expectedUrl)).Verifiable();
+
+            // Act
+            var aboutViewModel = new AboutViewModel(mockWindowService.Object, mockProcessService.Object);
+            aboutViewModel.NavigateProjectHomeUrlCommand.Execute(null);
+
+            // Assert
+            mockProcessService.Verify(p => p.StartWebUrl(expectedUrl));
+        }
+
+        [ Fact ]
+        public void NavigateContactUrl_Execute()
+        {
+            // Arrange
+            var expectedUrl = "https://twitter.com/invvard";
+            var mockWindowService = new Mock<IWindowService>();
+            var mockProcessService = new Mock<IProcessService>();
+            mockProcessService.Setup(p => p.StartWebUrl(expectedUrl)).Verifiable();
+
+            // Act
+            var aboutViewModel = new AboutViewModel(mockWindowService.Object, mockProcessService.Object);
+            aboutViewModel.NavigateContactUrlCommand.Execute(null);
+
+            // Assert
+            mockProcessService.Verify(p => p.StartWebUrl(expectedUrl));
         }
     }
 }
