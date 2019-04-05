9989580347a31a6c10c51d1659843745651c5157
Invvard <7305493+Invvard@users.noreply.github.com>
2019-04-04 20:53:46 -0400

    Remove warning about ClassNeverInstantiated

--- src/InvvardDev.EZLayoutDisplay.Desktop/Service/Design/ProcessService.cs
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Service/Design/ProcessService.cs
@@ -2,11 +2,12 @@
 
 namespace InvvardDev.EZLayoutDisplay.Desktop.Service.Design
 {
+    // ReSharper disable once ClassNeverInstantiated.Global : Instantiated in ViewModelLocator
     public class ProcessService : IProcessService
     {
         public void StartWebUrl(string url)
         {
-            return;
+
         }
     }
 }
