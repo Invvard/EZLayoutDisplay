0bfc829604fad12fb346de94b818ef5b378ece27
Invvard <7305493+Invvard@users.noreply.github.com>
2019-05-07 22:14:14 -0400

    Add the ErgoDox tag model

---
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Model/ErgodoxTag.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace InvvardDev.EZLayoutDisplay.Desktop.Model
+{
+    public class ErgodoxTag
+    {
+        /// <summary>
+        /// Gets or sets the tag name.
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
