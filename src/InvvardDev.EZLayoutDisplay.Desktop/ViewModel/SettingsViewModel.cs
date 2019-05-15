9fc9a68c1e32df1982238de6047381c5d9ed0453
Invvard <7305493+Invvard@users.noreply.github.com>
2019-05-14 22:22:36 -0400

    Expose LayoutInfo properties

--- src/InvvardDev.EZLayoutDisplay.Desktop/ViewModel/SettingsViewModel.cs
+++ src/InvvardDev.EZLayoutDisplay.Desktop/ViewModel/SettingsViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -28,6 +30,15 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.ViewModel
         private ICommand _updateLayoutCommand;
         private ICommand _closeSettingsCommand;
         private ICommand _cancelSettingsCommand;
+
+        private string _layoutTitle;
+        private string _keyboardModel;
+        private string _layoutStatus;
+        private ObservableCollection<string> _tags;
+        private ObservableCollection<string> _layers;
+        private Uri _hexFileUri;
+        private Uri _sourcesZipUri;
+        private bool _layoutIsCompiled;
 
         private string _altModifierLabel;
         private string _ctrlModifierLabel;
@@ -77,7 +88,7 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.ViewModel
 
         public string LayoutInfoGroupName { get; set; }
 
-        public string LayoutNameLabel { get; set; }
+        public string LayoutTitleLabel { get; set; }
 
         public string KeyboardModelLabel { get; set; }
 
@@ -111,6 +122,36 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.ViewModel
                 UpdateButtonCanExecute();
                 UpdateErgoDoxInfo();
             }
+        }
+
+        public string LayoutTitle
+        {
+            get => _layoutTitle;
+            private set => Set(ref _layoutTitle, value);
+        }
+
+        public string KeyboardModel
+        {
+            get => _keyboardModel;
+            private set => Set(ref _keyboardModel, value);
+        }
+
+        public string LayoutStatus
+        {
+            get => _layoutStatus;
+            private set => Set(ref _layoutStatus, value);
+        }
+
+        public ObservableCollection<string> Tags
+        {
+            get => _tags;
+            private set => Set(ref _tags, value);
+        }
+
+        public ObservableCollection<string> Layers
+        {
+            get => _layers;
+            private set => Set(ref _layers, value);
         }
 
         public string HotkeyTitleLabel { get; set; }
@@ -168,7 +209,7 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.ViewModel
             LayoutUrlLabel = "Configurator URL to your layout :";
 
             LayoutInfoGroupName = "Layout information";
-            LayoutNameLabel = "Name :";
+            LayoutTitleLabel = "Title :";
             KeyboardModelLabel = "Keyboard model :";
             TagsLabel = "Tags :";
             StatusLabel = "Layout status :";
@@ -250,13 +291,68 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.ViewModel
             return isDirty;
         }
 
-        private void UpdateErgoDoxInfo()
+        private async void UpdateErgoDoxInfo()
         {
             Logger.TraceMethod();
 
             var layoutHashId = ExtractLayoutHashId(LayoutUrlContent);
 
-            var layoutInfo = _layoutService.GetLayoutInfo(layoutHashId);
+            try
+            {
+                var layoutInfo = await _layoutService.GetLayoutInfo(layoutHashId);
+                Logger.Debug("LayoutInfo = {@value0}", layoutInfo);
+                UpdateLayoutInfo(layoutInfo);
+            }
+            catch (ArgumentNullException anex)
+            {
+                Logger.Error(anex);
+                _windowService.ShowWarning(anex.Message);
+            }
+            catch (ArgumentException aex)
+            {
+                Logger.Error(aex);
+                _windowService.ShowWarning(aex.Message);
+            }
+        }
+
+        private void UpdateLayoutInfo(ErgodoxLayout layoutInfo)
+        {
+            Logger.TraceMethod();
+
+            LayoutTitle = layoutInfo.Title;
+
+            if (layoutInfo.Tags != null && layoutInfo.Tags.Any())
+            {
+                Tags = new ObservableCollection<string>(layoutInfo.Tags.Select(t => t.Name));
+            }
+
+            if (layoutInfo.Revisions.Any())
+            {
+                var revision = layoutInfo.Revisions.First();
+
+                KeyboardModel = revision.Model;
+
+                UpdateLayoutButtons(revision);
+
+                Layers = new ObservableCollection<string>(revision.Layers.Select(l => l.ToString()));
+            }
+        }
+
+        private void UpdateLayoutButtons(Revision revision)
+        {
+            Logger.TraceMethod();
+
+            _layoutIsCompiled = Uri.IsWellFormedUriString(revision.HexUrl, UriKind.Absolute) && Uri.IsWellFormedUriString(revision.SourcesUrl, UriKind.Absolute);
+
+            if (!_layoutIsCompiled)
+            {
+                LayoutStatus = "Not compiled";
+                return;
+            }
+
+            LayoutStatus = "Compiled";
+            _hexFileUri = new Uri(revision.HexUrl);
+            _sourcesZipUri = new Uri(revision.SourcesUrl);
         }
 
         private async Task UpdateLayout()
