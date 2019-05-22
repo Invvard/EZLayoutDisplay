2b174c38d5e43eb2f11a80e71837874972eab2a7
Invvard <7305493+Invvard@users.noreply.github.com>
2019-05-21 22:36:15 -0400

    Rollback removal

--- src/InvvardDev.EZLayoutDisplay.Tests/ViewModel/SettingsViewModelTest.cs
+++ src/InvvardDev.EZLayoutDisplay.Tests/ViewModel/SettingsViewModelTest.cs
@@ -29,7 +29,16 @@ namespace InvvardDev.EZLayoutDisplay.Tests.ViewModel
             //Assert
             Assert.Equal("Settings", settingsViewModel.WindowTitle);
             Assert.Equal("Configurator URL to your layout :", settingsViewModel.LayoutUrlLabel);
+            Assert.Equal("Apply", settingsViewModel.ApplyCommandLabel);
+            Assert.Equal("Close", settingsViewModel.CloseCommandLabel);
+            Assert.Equal("Cancel", settingsViewModel.CancelCommandLabel);
             Assert.Equal(tbContentInitial, settingsViewModel.LayoutUrlContent);
+            Assert.Equal("Hotkey to display layout", settingsViewModel.HotkeyTitleLabel);
+            Assert.Equal("ALT", settingsViewModel.AltModifierLabel);
+            Assert.Equal("CTRL", settingsViewModel.CtrlModifierLabel);
+            Assert.Equal("SHIFT", settingsViewModel.ShiftModifierLabel);
+            Assert.Equal("Windows", settingsViewModel.WindowsModifierLabel);
+            Assert.Equal("Update", settingsViewModel.UpdateCommandLabel);
         }
 
         [ Theory ]
