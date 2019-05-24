88e6579b7dee7128236d42d9317c1f2a835f4ba0
Invvard <7305493+Invvard@users.noreply.github.com>
2019-05-23 21:19:20 -0400

    Update the constructor unit test

--- src/InvvardDev.EZLayoutDisplay.Tests/ViewModel/SettingsViewModelTest.cs
+++ src/InvvardDev.EZLayoutDisplay.Tests/ViewModel/SettingsViewModelTest.cs
@@ -29,16 +29,24 @@ namespace InvvardDev.EZLayoutDisplay.Tests.ViewModel
             //Assert
             Assert.Equal("Settings", settingsViewModel.WindowTitle);
             Assert.Equal("Configurator URL to your layout :", settingsViewModel.LayoutUrlLabel);
+            Assert.Equal("Layout information", settingsViewModel.LayoutInfoGroupName);
+            Assert.Equal("Title :", settingsViewModel.LayoutTitleLabel);
+            Assert.Equal("Keyboard model :", settingsViewModel.KeyboardModelLabel);
+            Assert.Equal("Tags :", settingsViewModel.TagsLabel);
+            Assert.Equal("Layout status :", settingsViewModel.StatusLabel);
+            Assert.Equal("Layers :", settingsViewModel.LayersLabel);
+            Assert.Equal("HEX File", settingsViewModel.HexFileCommandLabel);
+            Assert.Equal("Sources zip", settingsViewModel.SourcesZipCommandLabel);
             Assert.Equal("Apply", settingsViewModel.ApplyCommandLabel);
+            Assert.Equal("Update", settingsViewModel.UpdateCommandLabel);
             Assert.Equal("Close", settingsViewModel.CloseCommandLabel);
             Assert.Equal("Cancel", settingsViewModel.CancelCommandLabel);
-            Assert.Equal(tbContentInitial, settingsViewModel.LayoutUrlContent);
             Assert.Equal("Hotkey to display layout", settingsViewModel.HotkeyTitleLabel);
             Assert.Equal("ALT", settingsViewModel.AltModifierLabel);
             Assert.Equal("CTRL", settingsViewModel.CtrlModifierLabel);
             Assert.Equal("SHIFT", settingsViewModel.ShiftModifierLabel);
             Assert.Equal("Windows", settingsViewModel.WindowsModifierLabel);
-            Assert.Equal("Update", settingsViewModel.UpdateCommandLabel);
+            Assert.Equal(tbContentInitial, settingsViewModel.LayoutUrlContent);
         }
 
         [ Theory ]
