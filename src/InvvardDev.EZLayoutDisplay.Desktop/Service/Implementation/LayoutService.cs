cb2e15cd413f2615fd63f7c666a890680dcb6d53
Invvard <7305493+Invvard@users.noreply.github.com>
2019-07-22 14:31:49 -0400

    Update the ILayoutService implementation

--- src/InvvardDev.EZLayoutDisplay.Desktop/Service/Implementation/LayoutService.cs
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Service/Implementation/LayoutService.cs
@@ -40,7 +40,7 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.Service.Implementation
         }
 
         /// <inheritdoc />
-        public async Task<ErgodoxLayout> GetErgodoxLayout(string layoutHashId)
+        public async Task<ErgodoxLayout> GetErgodoxLayout(string layoutHashId, string layoutRevisionId)
         {
             Logger.TraceMethod();
             Logger.DebugInputParam(nameof(layoutHashId), layoutHashId);
