98ad4a1fdacce6c92ccc2d00a51357fd40dfb645
Invvard <7305493+Invvard@users.noreply.github.com>
2022-01-06 14:16:53 -0500

    Update ZSA keyboard datamodel

---
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Model/ErgodoxTap.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+
+namespace InvvardDev.EZLayoutDisplay.Desktop.Model
+{
+    public class ErgodoxKeyFeature
+    {
+        /// <summary>
+        /// Gets or sets the key code.
+        /// </summary>
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Color.
+        /// </summary>
+        [JsonProperty("color", NullValueHandling = NullValueHandling.Ignore)]
+        public string Color { get; set; }
+
+        /// <summary>
+        /// Gets or sets the layer index.
+        /// </summary>
+        [JsonProperty("layer", NullValueHandling = NullValueHandling.Ignore)]
+        public int? Layer { get; set; }
+
+        /// <summary>
+        /// Gets or sets the primary command.
+        /// </summary>
+        [JsonProperty("macro", NullValueHandling = NullValueHandling.Ignore)]
+        public string Macro { get; set; }
+
+        /// <summary>
+        /// Gets or sets the modifiers.
+        /// </summary>
+        [JsonProperty("modifiers", NullValueHandling = NullValueHandling.Ignore)]
+        public ErgodoxModifiers Modifiers { get; set; }
+
+    }
+}
