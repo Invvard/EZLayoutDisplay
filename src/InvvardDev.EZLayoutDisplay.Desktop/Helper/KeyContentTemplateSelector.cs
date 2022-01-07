d00d03952cf4fe59c481e5a132846cda381ec125
Pierre Cavaroc <pierre.cavaroc@hiloenergie.com>
2022-01-07 08:32:22 -0500

    Update Key display type data templates

--- src/InvvardDev.EZLayoutDisplay.Desktop/Helper/KeyContentTemplateSelector.cs
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Helper/KeyContentTemplateSelector.cs
@@ -8,33 +8,44 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.Helper
 {
     public class KeyContentTemplateSelector : DataTemplateSelector
     {
-        public DataTemplate SimpleLabelDataTemplate { private get; set; }
-        public DataTemplate ModifierOnTopDataTemplate { private get; set; }
-        public DataTemplate ModifierUnderDataTemplate { private get; set; }
+        public DataTemplate LabelDataTemplate { private get; set; }
+        public DataTemplate LabelModDataTemplate { private get; set; }
+        public DataTemplate TapHoldDataTemplate { private get; set; }
+        public DataTemplate TapModHoldDataTemplate { private get; set; }
+        public DataTemplate TapHoldModDataTemplate { private get; set; }
+        public DataTemplate TapModHoldModDataTemplate { private get; set; }
 
         public override DataTemplate SelectTemplate(object item, DependencyObject container)
         {
-            DataTemplate template = SimpleLabelDataTemplate;
+            DataTemplate template = LabelDataTemplate;
 
             if (!(item is KeyTemplate key)) return template;
 
             switch (key.EZKey.DisplayType)
             {
                 case KeyDisplayType.None:
-                case KeyDisplayType.SimpleLabel:
-                    template = SimpleLabelDataTemplate;
-
+                case KeyDisplayType.Tap:
+                case KeyDisplayType.Hold:
+                case KeyDisplayType.CustomLabel:
+                    template = LabelDataTemplate;
                     break;
-                case KeyDisplayType.ModifierOnTop:
-                    template = ModifierOnTopDataTemplate;
-
+                case KeyDisplayType.TapMod:
+                case KeyDisplayType.HoldMod:
+                    template = LabelModDataTemplate;
                     break;
-                case KeyDisplayType.ModifierUnder:
-                    template = ModifierUnderDataTemplate;
-
+                case KeyDisplayType.TapHold:
+                    template = TapHoldDataTemplate;
+                    break;
+                case KeyDisplayType.TapModHold:
+                    template = TapModHoldDataTemplate;
+                    break;
+                case KeyDisplayType.TapHoldMod:
+                    template = TapHoldModDataTemplate;
+                    break;
+                case KeyDisplayType.TapModHoldMod:
+                    template = TapModHoldModDataTemplate;
                     break;
                 default:
-
                     throw new ArgumentOutOfRangeException();
             }
 
