979cf0911c24d2e85c1bca566bb8a42151b21877
Invvard <7305493+Invvard@users.noreply.github.com>
2022-01-19 19:17:57 -0500

    Refactor key templates

--- src/InvvardDev.EZLayoutDisplay.Desktop/Helper/KeyContentTemplateSelector.cs
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Helper/KeyContentTemplateSelector.cs
@@ -8,20 +8,14 @@ namespace InvvardDev.EZLayoutDisplay.Desktop.Helper
 {
     public class KeyContentTemplateSelector : DataTemplateSelector
     {
-        public DataTemplate CustomLabelDataTemplate { private get; set; }
         public DataTemplate SingleFeatureDataTemplate { private get; set; }
         public DataTemplate DoubleFeatureDataTemplate { private get; set; }
 
         public override DataTemplate SelectTemplate(object item, DependencyObject container)
-        {
-            if (!(item is KeyTemplate key)) return CustomLabelDataTemplate;
-
-            return key.EZKey.DisplayMode switch
+            => item switch
             {
-                KeyDisplayMode.SingleFeature => SingleFeatureDataTemplate,
-                KeyDisplayMode.DoubleFeature => DoubleFeatureDataTemplate,
-                _ => CustomLabelDataTemplate
+                KeyTemplate { Key.DisplayMode: KeyDisplayMode.DualFunction } => DoubleFeatureDataTemplate,
+                _ => SingleFeatureDataTemplate
             };
-        }
     }
 }
---
+++ src/InvvardDev.EZLayoutDisplay.Desktop/Helper/KeyFeatureTemplateSelector.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using InvvardDev.EZLayoutDisplay.Desktop.Model.Ez.Content;
+
+namespace InvvardDev.EZLayoutDisplay.Desktop.Helper
+{
+    public class KeyFeatureTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate SimpleLabelDataTemplate { private get; set; }
+        public DataTemplate GlyphDataTemplate { private get; set; }
+        public DataTemplate ModdedGlyphDataTemplate { private get; set; }
+        public DataTemplate LayerDataTemplate { private get; set; }
+        public DataTemplate ColorPickerDataTemplate { private get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            return item switch
+            {
+                Glyph glyph when !string.IsNullOrWhiteSpace(glyph.Modifier) => ModdedGlyphDataTemplate,
+                Glyph glyph when string.IsNullOrWhiteSpace(glyph.Modifier) => GlyphDataTemplate,
+                Layer => LayerDataTemplate,
+                ColorPicker => ColorPickerDataTemplate,
+                _ => SimpleLabelDataTemplate
+            };
+        }
+    }
+}
